c5dcf454bed4602a1963dc3adff84b6e2ee71ec8
Bert Huijben <rhuijben@users.noreply.github.com>
2007-01-07 12:58:55 +0000

    Added FullPath property (caching result of common operation) and use it in the test program

--- src/TurtleSvn.Console/Program.cs
+++ src/TurtleSvn.Console/Program.cs
@@ -4,6 +4,7 @@ using System.Text;
 using TurtleSvn;
 using System.Reflection;
 using TurtleSvn.Security;
+using System.IO;
 
 namespace TurtleSvn
 {
@@ -24,7 +25,7 @@ namespace TurtleSvn
 
 			_client.LoadConfigurationDefault();
 			_client.Authenticator.AddSubversionFileHandlers();
-			_client.Authenticator.UsernamePasswordHandlers += new EventHandler<TurtleSvn.Security.SvnUsernamePasswordArgs>(Authentication_UsernamePasswordHandlers);
+			_client.Authenticator.UsernamePasswordHandlers += new SvnAuthenticationHandler<SvnUsernamePasswordArgs>(Authenticator_UsernamePasswordHandlers);
 
 			//_client.Authenticator.
 			//_client.Authentication.
@@ -33,32 +34,46 @@ namespace TurtleSvn
 			_client.ClientNotify += new EventHandler<SvnClientNotifyEventArgs>(OnClientNotify);
 			_client.ClientProgress += new EventHandler<SvnClientProgressEventArgs>(OnClientProgress);
 
-			SvnTarget target;
+			//SvnTarget target;
 
-			if (SvnTarget.TryParse("f:\\projects@{2000-03-20T13:02:03}", out target))
-				Console.WriteLine(target.ToString());
+			string _tmpDir = "f:\\svn-test-location";
 
-			if (SvnTarget.TryParse("http://www.vmoo.com/@HEAD", out target))
-				Console.WriteLine(target.ToString());
+			if (Directory.Exists("f:\\svn-test-location"))
+			{
+				DirectoryInfo dir = new DirectoryInfo(_tmpDir);
 
-			Console.WriteLine(((SvnUriTarget)new Uri("http://qqn.nl/")).ToString());
+				foreach (FileInfo file in dir.GetFiles("*", SearchOption.AllDirectories))
+				{
+					if ((file.Attributes & FileAttributes.ReadOnly) != 0)
+						file.Attributes = FileAttributes.Normal;
+				}
+
+				Directory.Delete("f:\\svn-test-location", true);
+			}
+			Directory.CreateDirectory("f:\\svn-test-location");
 
 			long ver;
+			_client.CheckOut(new Uri("https://studioturtle.googlecode.com/svn/trunk/src/TurtleSvn/"), "f:\\svn-test-location\\gc", out ver);
+			_client.Update("f:\\svn-test-location\\gc");
 			//_client.CheckOut(new Uri("file:///F:/_local/repos/"), "f:\\test-co", out ver);
-			_client.Update(@"g:\Projects\SvnDotNet", new SvnUpdateArgs(), out ver);
+			//_client.Update(@"g:\Projects\SvnDotNet", new SvnUpdateArgs(), out ver);
+			//long rev;
+			
 		}
 
-		void Authentication_UsernamePasswordHandlers(object sender, TurtleSvn.Security.SvnUsernamePasswordArgs e)
+		bool Authenticator_UsernamePasswordHandlers(object sender, TurtleSvn.Security.SvnUsernamePasswordArgs e)
 		{
-			
+			return false;
 		}
 
 		void OnClientProgress(object sender, SvnClientProgressEventArgs e)
 		{
+
 		}
 
 		void OnClientNotify(object sender, SvnClientNotifyEventArgs e)
 		{
+			Console.WriteLine(e.FullPath);
 		}
 
 		void OnClientCancel(object sender, SvnClientCancelEventArgs e)
