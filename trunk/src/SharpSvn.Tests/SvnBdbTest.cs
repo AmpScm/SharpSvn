2308d65288d841c5031f367dd8afd1ec7fbe984f
bhuijben <bhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2007-09-09 22:46:14 +0000

    Verified project in VS 2008 beta 2

---
+++ trunk/src/SharpSvn.Tests/SvnBdbTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.IO;
+
+namespace SharpSvn.Tests
+{
+    [TestFixture]
+    public class SvnBdbTest : SvnTestBase
+    {
+
+        [Test]
+        public void CreateBdbRepos()
+        {
+            string path = Path.Combine(TestPath, "BdbRepos");
+
+            if(Directory.Exists(path))
+                ForcedDeleteDirectory(path);
+
+            SvnRepositoryClient reposClient = new SvnRepositoryClient();
+            
+            reposClient.CreateRepository(path, new SvnCreateRepositoryArgs(){ RepositoryType = SvnRepositoryFilesystem.BerkeleyDB });
+        }
+
+    }
+}
