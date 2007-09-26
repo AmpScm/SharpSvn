7926ca1428bd76ba07a5d38ef65b673e71b7beaf
bhuijben <bhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2007-09-26 15:19:58 +0000

    Added SvnClientUIHandler

---
+++ trunk/src/SharpSvn.UI/SvnClientUIHandler.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using SharpSvn.Security;
+using SharpSvn.UI.Authentication;
+
+namespace SharpSvn.UI
+{
+	sealed class SvnClientUIHandler
+	{
+		readonly IWin32Window _window;
+
+		sealed class ZeroWindowHandle : IWin32Window
+		{
+			public IntPtr Handle
+			{
+				get { return IntPtr.Zero; }
+			}
+		}
+
+		public SvnClientUIHandler(IWin32Window window)
+		{
+			_window = window ?? new ZeroWindowHandle();
+		}
+
+		internal void Bind(SvnClient svnClient)
+		{
+			svnClient.Authenticator.UsernameHandlers += new EventHandler<SharpSvn.Security.SvnUsernameEventArgs>(DialogUsernameHandler);
+			svnClient.Authenticator.UsernamePasswordHandlers += new EventHandler<SharpSvn.Security.SvnUsernamePasswordEventArgs>(DialogUsernamePasswordHandler);
+			svnClient.Authenticator.SslClientCertificateHandlers += new EventHandler<SharpSvn.Security.SvnSslClientCertificateEventArgs>(DialogSslClientCertificateHandler);
+			svnClient.Authenticator.SslClientCertificatePasswordHandlers += new EventHandler<SharpSvn.Security.SvnSslClientCertificatePasswordEventArgs>(DialogSslClientCertificatePasswordHandler);
+			svnClient.Authenticator.SslServerTrustHandlers += new EventHandler<SharpSvn.Security.SvnSslServerTrustEventArgs>(DialogSslServerTrustHandler);
+		}
+
+		void DialogUsernameHandler(Object sender, SvnUsernameEventArgs e)
+		{
+			String username;
+			bool save;
+
+			if (SharpSvnGui.AskUsername(_window.Handle, "Connect to Subversion", e.Realm, e.MaySave, out username, out save))
+			{
+				e.Username = username;
+				e.Save = save && e.MaySave;
+			}
+			else
+				e.Break = true;
+		}
+
+		void DialogUsernamePasswordHandler(Object sender, SvnUsernamePasswordEventArgs e)
+		{
+			String username;
+			String password;
+			bool save;
+
+			if (SharpSvnGui.AskUsernamePassword(_window.Handle, "Connect to Subversion", e.Realm, e.InitialUsername, e.MaySave, out username, out password, out save))
+			{
+				e.Username = username;
+				e.Password = password;
+				e.Save = save && e.MaySave;
+			}
+			else
+				e.Break = true;
+		}
+
+		void DialogSslServerTrustHandler(Object sender, SvnSslServerTrustEventArgs e)
+		{
+			bool save;
+
+			ServerCertificateInfo sci = new ServerCertificateInfo();
+
+			sci.InvalidCommonName = (0 != (int)(e.Failures & SvnCertificateTrustFailures.CommonNameMismatch));
+			sci.NoTrustedIssuer = 0 != (int)(e.Failures & SvnCertificateTrustFailures.UnknownCertificateAuthority);
+			sci.TimeError = 0 != (int)(e.Failures & (SvnCertificateTrustFailures.CertificateExpired | SvnCertificateTrustFailures.CertificateNotValidYet));
+
+			sci.Hostname = e.CommonName;
+			sci.Fingerprint = e.FingerPrint;
+			sci.Certificate = e.CertificateValue;
+			sci.Issuer = e.Issuer;
+			sci.ValidFrom = e.ValidFrom;
+			sci.ValidTo = e.ValidUntil;
+
+			bool accept = false;
+			if (SharpSvnGui.AskServerCertificateTrust(_window.Handle, "Connect to Subversion", e.Realm, sci, e.MaySave, out accept, out save))
+			{
+				e.AcceptedFailures = accept ? e.Failures : SvnCertificateTrustFailures.None;
+				e.Save = save && e.MaySave;
+			}
+			else
+				e.Break = true;
+		}
+
+		void DialogSslClientCertificateHandler(Object sender, SvnSslClientCertificateEventArgs e)
+		{
+			String file;
+			bool save;
+
+			if (SharpSvnGui.AskClientCertificateFile(_window.Handle, "Connect to Subversion", e.Realm, e.MaySave, out file, out save))
+			{
+				e.CertificateFile = file;
+				e.Save = save && e.MaySave;
+			}
+			else
+				e.Break = true;
+		}
+
+		void DialogSslClientCertificatePasswordHandler(Object sender, SvnSslClientCertificatePasswordEventArgs e)
+		{
+			String password;
+			bool save;
+
+			if (SharpSvnGui.AskClientCertificatePassPhrase(_window.Handle, "Connect to Subversion", e.Realm, e.MaySave, out password, out save))
+			{
+				e.Password = password;
+				e.Save = save && e.MaySave;
+			}
+			else
+				e.Break = true;
+		}
+	}
+}
