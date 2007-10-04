e2f583d24dc7bd2bc075affaca8bc7ac59fea454
Bert Huijben <rhuijben@users.noreply.github.com>
2007-10-04 22:26:02 +0000

    Added 2 missing files

---
+++ src/SharpSvn.UI/SharpSvnUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SharpSvn.UI
+{
+	/// <summary>
+	/// Helper methods to bind a user interface to an <see cref="SvnClient"/>
+	/// </summary>
+	public static class SharpSvnUI
+	{
+		/// <summary>
+		/// Binds the default userinterface to the <see cref="SvnClient"/> using <paramref name="parentWindow"/> as parent for the popup windows
+		/// </summary>
+		/// <param name="svnClient"></param>
+		/// <param name="parentWindow"></param>
+		public static void Bind(SvnClient svnClient, IWin32Window parentWindow)
+		{
+			if (svnClient == null)
+				throw new ArgumentNullException("svnClient");
+
+			SvnClientUIHandler handler = new SvnClientUIHandler(parentWindow);
+
+			handler.Bind(svnClient);
+		}
+	}
+}
