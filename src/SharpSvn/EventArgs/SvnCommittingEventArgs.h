7d8153e6b8bc83d5d64be8f9e72ecb0cc2d17619
Bert Huijben <rhuijben@users.noreply.github.com>
2008-02-15 13:18:38 +0000

    Updated layout for vs2005 and fixed properties of files

--- src/SharpSvn/EventArgs/SvnCommittingEventArgs.h
+++ src/SharpSvn/EventArgs/SvnCommittingEventArgs.h
@@ -1,4 +1,4 @@
-// $Id: SvnClientEventArgs_Global.h 266 2008-02-14 13:24:29Z bhuijben $
+// $Id$
 // Copyright (c) SharpSvn Project 2007
 // The Sourcecode of this project is available under the Apache 2.0 license
 // Please read the SharpSvnLicense.txt file for more details
