e6f3576b3eb24d37550ccadb705d2f45aba4eacd
bhuijben <bhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-02-21 11:46:20 +0000

    Removed SharpDns from trunk. Development will shift to a branch or other repository.
    Imports build was removed in r325

--- trunk/src/SharpDns/DnsSection.cpp
+++
@@ -1,8 +0,0 @@
-// $Id$
-// Copyright (c) SharpSvn Project 2007 
-// The Sourcecode of this project is available under the Apache 2.0 license
-// Please read the SharpSvnLicense.txt file for more details
-#include "StdAfx.h"
-#include "DnsAll.h"
-
-using namespace SharpDns;
