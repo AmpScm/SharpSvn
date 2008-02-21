53f872aa557fc38d2ddefffb722aff70d4b94c05
srijken <srijken@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-02-21 15:50:23 +0000

    add -Credential parameter to all commands

--- trunk/contrib/PowerShell/SharpSvn/Commands/SvnCommandBase.cs
+++ trunk/contrib/PowerShell/SharpSvn/Commands/SvnCommandBase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Management.Automation;
+using System.Net;
 
 namespace SharpSvn.PowerShell
 {
@@ -12,6 +13,13 @@ namespace SharpSvn.PowerShell
         protected SvnCommandBase()
             : base()
         { }
+
+        [Parameter(HelpMessage="The default credential to use for authentication.")]
+        public ICredentials Credential
+        {
+            get { return Client.Authenticator.DefaultCredentials; }
+            set { Client.Authenticator.DefaultCredentials = value; }
+        }
 
         SvnClient _client;
         protected SvnClient Client
@@ -25,6 +33,9 @@ namespace SharpSvn.PowerShell
                     {
                         WriteError(new ErrorRecord(e.Exception, e.Exception.SvnErrorCode.ToString(), ErrorCategory.FromStdErr, null));
                         e.Cancel = true;
+                    };
+                    _client.Notify += delegate(object sender, SvnNotifyEventArgs e)
+                    {
                     };
                 }
                 return _client;
