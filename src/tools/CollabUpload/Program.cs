edf34b588f02362bd38793a107fad9d9adabbfa0
Bert Huijben <rhuijben@users.noreply.github.com>
2008-05-07 18:10:08 +0000

    Added simple file upload tool for daily builds of AnkhSVN (and regular updates of SharpSvn) Currently contains at least one bug that breaks the upload

---
+++ src/tools/CollabUpload/Program.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Web;
+using QQn.TurtleUtils.IO;
+
+namespace CollabUpload
+{
+    public class Program
+    {
+        class Args
+        {
+            public string Site;
+            public int Folder;
+
+            public string UserName;
+            public string Password;
+            public string Name;
+            public string Status;
+            public string Description;
+            public readonly List<string> Files = new List<string>();
+        }
+
+        static void Main(string[] args)
+        {
+            Args a = Parse(args);
+            if (a != null)
+                Run(a);
+        }
+
+        static Args Parse(string[] args)
+        {
+            Args aa = new Args();
+            int i;
+
+            for (i = 0; i < args.Length; i++)
+            {
+                string key = args[i];
+                if (!string.IsNullOrEmpty(key) && key[0] == '-')
+                {
+                    if (args[i] == "--")
+                    {
+                        i++;
+                        break;
+                    }
+
+                    bool argAvailable = i + 1 < args.Length;
+
+                    string value = argAvailable ? args[i + 1] : null;
+                    switch (key)
+                    {
+                        case "--site":
+                            if (!argAvailable)
+                                return ArgError("--host requires argument");
+                            aa.Site = value.Contains(".") ? value : string.Format("{0}.open.collab.net", value);
+                            i++;
+                            break;
+                        case "--folder":
+                            if (!argAvailable || !int.TryParse(value, out aa.Folder))
+                                return ArgError("--folder requires integer argument");
+                            i++;
+                            break;
+                        case "--username":
+                            if (!argAvailable)
+                                return ArgError("--username requires argument");
+                            aa.UserName = value;
+                            i++;
+                            break;
+                        case "--password":
+                            if (!argAvailable)
+                                return ArgError("--password requires argument");
+                            aa.Password = value;
+                            i++;
+                            break;
+                        case "--name":
+                            if (!argAvailable)
+                                return ArgError("--name requires argument");
+                            aa.Name = value;
+                            i++;
+                            break;
+                        case "--description":
+                            if (!argAvailable)
+                                return ArgError("--description requires argument");
+                            aa.Description = value;
+                            i++;
+                            break;
+                        case "--status":
+                            if (!argAvailable)
+                                return ArgError("--status requires argument");
+                            aa.Status = value;
+                            i++;
+                            break;
+                        default:
+                            return ArgError(string.Format("Unknown argument: {0}", value));
+                    }
+                }
+                else
+                    aa.Files.Add(key);
+            }
+
+            while (i < args.Length)
+                aa.Files.Add(args[i++]);
+
+            return aa;
+        }
+
+        static private Args ArgError(string error)
+        {
+            Console.Error.WriteLine("Error: {0}", error);
+            return null;
+        }
+
+        static Args Run(Args args)
+        {
+            CookieContainer cookBox = new CookieContainer();
+
+            string requestUri = string.Format("http://{0}/servlets/ProjectDocumentAdd?folderID={1}", args.Site, args.Folder);
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(requestUri);
+            wr.CookieContainer = cookBox;
+            string responseUri;
+            string text;
+
+            using (WebResponse response = wr.GetResponse())
+            {
+                responseUri = response.ResponseUri.ToString();
+                text = GetText(response);
+            }
+
+            if (responseUri != requestUri && responseUri.Contains("/Login"))
+            {
+                // Standard Collabnet login on secondary host..
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(text);
+
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("html", "http://www.w3.org/1999/xhtml");
+                XmlElement form = (XmlElement)doc.SelectSingleNode("//html:form[@id='loginform']", nsmgr);
+                StringBuilder sb = new StringBuilder();
+
+                wr = (HttpWebRequest)WebRequest.Create(new Uri(new Uri(responseUri), new Uri(form.GetAttribute("action"))));
+                wr.CookieContainer = cookBox;
+                using(WebFormWriter wfw = new WebFormWriter(wr, WebRequestPostDataEncoding.WwwFormUrlEncoded, "POST"))
+                {
+                    foreach (XmlElement el in form.SelectNodes(".//html:input[@name]", nsmgr))
+                    {
+                        string name = el.GetAttribute("name");
+
+                        switch (name.ToUpperInvariant())
+                        {
+                            case "LOGINID":
+                                wfw.AddValue(name, args.UserName);
+                                break;
+                            case "PASSWORD":
+                                wfw.AddValue(name, args.Password);
+                                break;
+                            default:
+                                string v = el.GetAttribute("value");
+                                wfw.AddValue(name, v ?? "");
+                                break;
+                        }
+                    }
+                }
+
+                using (WebResponse response = wr.GetResponse())
+                {
+                    responseUri = response.ResponseUri.ToString();
+                    text = GetText(response);
+                }
+
+                if (responseUri != requestUri)
+                    return ArgError("Invalid credentials specified");
+            }
+
+            // Ok, we are logged in; let's upload the files
+
+            foreach (string file in args.Files)
+            {
+                Console.WriteLine("Uploading {0}", Path.GetFileName(file));
+                wr = (HttpWebRequest)WebRequest.Create(requestUri + "&action=Add%20document");
+                wr.CookieContainer = cookBox;
+
+                using (WebFormWriter wfw = new WebFormWriter(wr, WebRequestPostDataEncoding.MultipartFormData, "POST"))
+                {
+                    wfw.AddValue("name", args.Name ?? Path.GetFileName(file));
+                    wfw.AddValue("status", args.Status ?? "Draft");
+                    wfw.AddValue("description", args.Description ?? "Description");
+                    //wfw.AddValue("initiallylocked", "");
+                    wfw.AddValue("type", "file");                    
+                    wfw.AddValue("textFormat", "pre");
+                    wfw.AddFile("file", file);                    
+                    //wfw.AddValue("docUrl", "");
+                    wfw.AddValue("submit", "submit");
+                    //wfw.AddValue("maxDepth", "");
+                }
+
+                using (WebResponse response = wr.GetResponse())
+                {
+                    text = GetText(response);
+
+                    if (text.Contains("\"error"))
+                    {
+                        Console.WriteLine("Upload failed");
+                        return null;
+                    }
+                }
+            }
+            return null;
+        }
+
+        static string GetText(WebResponse response)
+        {
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ src/tools/CollabUpload/WebFormWriter.cs
@@ -0,0 +1,324 @@
+﻿// This file is litterally copied from the TurtleUtils project: 
+// http://turtlebuild.googlecode.com/svn/trunk/src/Libraries/QQn.TurtleUtils/IO/WebFormWriter.cs
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace QQn.TurtleUtils.IO
+{
+    /// <summary>
+    /// The encoding of the webrequest data
+    /// </summary>
+    public enum WebRequestPostDataEncoding
+    {
+        /// <summary>
+        /// Normal web post
+        /// </summary>
+        WwwFormUrlEncoded = 0,
+
+        /// <summary>
+        /// Multipart post; optimized for binary data
+        /// </summary>
+        MultipartFormData
+    }
+    /// <summary>
+    /// Writer of form data for a <see cref="HttpWebRequest"/>
+    /// </summary>
+    public class WebFormWriter : IDisposable
+    {
+        readonly WebRequest _request;
+        readonly WebRequestPostDataEncoding _encoding;
+        Stream _webRequestStream;
+        StreamWriter _streamWriter;
+
+        static readonly string _topBoundary = Guid.NewGuid().ToString("N");
+        //static readonly string _subBoundary = Guid.NewGuid().ToString("N");
+        const string NetworkEol = "\r\n";
+
+        bool _nextField;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebFormWriter"/> class with method POST
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="encoding">The encoding.</param>
+        public WebFormWriter(WebRequest request, WebRequestPostDataEncoding encoding)
+            : this(request, encoding, "POST")
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebFormWriter"/> class.
+        /// </summary>
+        /// <param name="request">The request.</param>
+        /// <param name="encoding">The encoding.</param>
+        /// <param name="method">The method.</param>
+        public WebFormWriter(WebRequest request, WebRequestPostDataEncoding encoding, string method)
+        {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
+            _request = request;
+            _request.Method = method;
+            _encoding = encoding;
+
+            switch (encoding)
+            {
+                case WebRequestPostDataEncoding.WwwFormUrlEncoded:
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    break;
+                case WebRequestPostDataEncoding.MultipartFormData:
+                    request.ContentType = string.Format(CultureInfo.InvariantCulture, "multipart/form-data; boundary={0}", _topBoundary);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("encoding", encoding, "Unknown encoding");
+            }
+
+            _webRequestStream = request.GetRequestStream();
+            _streamWriter = new StreamWriter(_webRequestStream, Encoding.ASCII);
+        }
+
+        /// <summary>
+        /// Writes the boundary.
+        /// </summary>
+        void WriteBoundary()
+        {
+            switch (_encoding)
+            {
+                case WebRequestPostDataEncoding.MultipartFormData:
+                    _streamWriter.Write("--");
+                    _streamWriter.Write(_topBoundary);
+                    _streamWriter.Write(NetworkEol);
+                    break;
+                default:
+                    throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "WriteBoundary() is not supported for encoding {0}", _encoding));
+            }
+        }
+
+
+        /// <summary>
+        /// Adds the value.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void AddValue(string key, string value)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+            else if (value == null)
+                throw new ArgumentNullException("value");
+
+            switch (_encoding)
+            {
+                case WebRequestPostDataEncoding.WwwFormUrlEncoded:
+                    if (_nextField)
+                        _streamWriter.Write('&');
+                    else
+                        _nextField = true;
+
+                    WriteUrlEncoded(key);
+                    _streamWriter.Write('=');
+                    WriteUrlEncoded(value);
+                    break;
+                case WebRequestPostDataEncoding.MultipartFormData:
+                    AddValue(key, Encoding.UTF8.GetBytes(value));
+                    break;
+
+                default:
+                    throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "AddFile(String, String) is not supported for encoding {0}", _encoding));
+            }
+        }
+
+        /// <summary>
+        /// Adds the specified key-value-pair
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="value">The value.</param>
+        public void AddValue(string key, byte[] value)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+            else if (value == null)
+                throw new ArgumentNullException("value");
+
+            switch (_encoding)
+            {
+                case WebRequestPostDataEncoding.MultipartFormData:
+                    if (!IsSafeKeyName(key))
+                        throw new FormatException("Invalid character in key");
+
+                    WriteBoundary();
+                    _streamWriter.Write("Content-Disposition: form-data; name=\"{0}\"", key);
+                    _streamWriter.Write(NetworkEol);
+                    _streamWriter.Write(NetworkEol);
+
+                    _webRequestStream.Write(value, 0, value.Length);
+                    _streamWriter.Write(NetworkEol);
+                    break;
+                default:
+                    throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "AddValue(String, Byte[]) is not supported for encoding {0}", _encoding));
+            }
+        }
+
+        static bool IsSafeKeyName(string key)
+        {
+            foreach (char c in key)
+            {
+                if (!char.IsLetterOrDigit(c) || char.IsWhiteSpace(c))
+                    return false;
+            }
+            return true;
+        }
+
+        void WriteUrlEncoded(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                if (char.IsLetterOrDigit(value, i))
+                    _streamWriter.Write(value[i]);
+                else switch (value[i])
+                    {
+                        case ' ':
+                            _streamWriter.Write('+');
+                            break;
+                        default:
+                            foreach (byte b in _streamWriter.Encoding.GetBytes(new char[] { value[i] }))
+                            {
+                                _streamWriter.Write("%{0:X2}", b);
+                            }
+                            break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Adds the specified file with the application/octet-stream content-type
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="filename">The filename.</param>
+        public void AddFile(string key, string filename)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+            else if (string.IsNullOrEmpty(filename))
+                throw new ArgumentNullException("filename");
+
+            AddFile(key, filename, "application/octet-stream");
+        }
+
+        /// <summary>
+        /// Adds the specified file with the specified content-type
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="filename">The filename.</param>
+        /// <param name="contentType">Type of the content.</param>
+        public void AddFile(string key, string filename, string contentType)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+            else if (string.IsNullOrEmpty(filename))
+                throw new ArgumentNullException("filename");
+            else if (string.IsNullOrEmpty(contentType))
+                throw new ArgumentNullException("contentType");
+
+            using (FileStream fs = File.OpenRead(filename))
+            {
+                AddFile(key, filename, fs, contentType);
+            }
+        }
+
+        /// <summary>
+        /// Adds the stream with the specified filename and content-type
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <param name="filename">The filename.</param>
+        /// <param name="stream">The stream.</param>
+        /// <param name="contentType">Type of the content.</param>
+        public void AddFile(string key, string filename, Stream stream, string contentType)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException("key");
+            else if (string.IsNullOrEmpty(filename))
+                throw new ArgumentNullException("filename");
+            else if (string.IsNullOrEmpty(contentType))
+                throw new ArgumentNullException("contentType");
+            else if (stream == null)
+                throw new ArgumentNullException("stream");
+
+            filename = Path.GetFileName(filename);
+
+            switch (_encoding)
+            {
+                case WebRequestPostDataEncoding.MultipartFormData:
+                    if (!IsSafeKeyName(key))
+                        throw new FormatException("Invalid character in key");
+
+                    WriteBoundary();
+                    _streamWriter.Write("Content-Disposition: form-data; name=\"{0}\" filename=\"{1}\"", key, filename);
+                    _streamWriter.Write(NetworkEol);
+                    _streamWriter.Write("Content-Type: {0}", contentType);
+                    _streamWriter.Write(NetworkEol);
+                    _streamWriter.Write(NetworkEol);
+
+                    byte[] buffer = new byte[8192];
+                    int read;
+
+                    while (0 < (read = stream.Read(buffer, 0, buffer.Length)))
+                    {
+                        _webRequestStream.Write(buffer, 0, read);
+                    }
+
+                    _streamWriter.Write(NetworkEol);
+                    break;
+                default:
+                    throw new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, "AddFile is not supported for encoding {0}", _encoding));
+            }
+        }
+
+        #region IDisposable Members
+
+        [SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly")]
+        void IDisposable.Dispose()
+        {
+            Dispose(true);
+        }
+
+        /// <summary>
+        /// Releases resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                Close();
+        }
+
+        bool _closed;
+        /// <summary>
+        /// Closes the writer and the RequestStream of the <see cref="WebRequest"/>
+        /// </summary>
+        public virtual void Close()
+        {
+            if (_closed)
+                return;
+
+            _closed = true;
+            _streamWriter.Dispose();
+
+            if (_webRequestStream != null)
+            {
+                _webRequestStream.Close();
+                _webRequestStream = null;
+            }
+        }
+
+        #endregion
+    }
+}
