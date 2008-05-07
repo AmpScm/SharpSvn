230ea2afe2afd2fcb9e54351125d6c653976a3fd
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-05-07 19:42:30 +0000

    Added missing testfile

---
+++ trunk/src/SharpSvn.Tests/Commands/CreateRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace SharpSvn.Tests.Commands
+{
+    [TestFixture]
+    public class CreateRepositoryTests : TestBase
+    {
+        [Test]
+        public void CreateFsFsRepository()
+        {
+            using (SvnRepositoryClient reposClient = new SvnRepositoryClient())
+            {
+                SvnCreateRepositoryArgs cra = new SvnCreateRepositoryArgs();
+                cra.RepositoryCompatibility = SvnRepositoryCompatibility.Default;
+                reposClient.CreateRepository(GetTempDir(), cra);
+            }
+        }
+
+        [Test]
+        public void CreateBdbRepository()
+        {
+            using (SvnRepositoryClient reposClient = new SvnRepositoryClient())
+            {
+                SvnCreateRepositoryArgs cra = new SvnCreateRepositoryArgs();
+                cra.RepositoryType = SvnRepositoryFileSystem.BerkeleyDB;
+                cra.RepositoryCompatibility = SvnRepositoryCompatibility.Default;
+                reposClient.CreateRepository(GetTempDir(), cra);
+            }
+        }
+    }
+}
