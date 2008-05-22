58a1aa48be9adbfc7bbba167cff0655fe92a21b0
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-05-22 09:50:30 +0000

    Commit work in progress on simple commandline client

---
+++ trunk/src/ShSvn/CommandBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShSvn
+{
+    public abstract class CommandBase
+    {
+        public abstract string[] Commands { get; }
+
+        public abstract int Run(string command, string[] args, CommandState e);
+
+        public void ShowHelp()
+        {
+            Console.WriteLine("Usage: ShSvn [<options>] {0}", Commands[0]);
+        }
+    }
+}
---
+++ trunk/src/ShSvn/CommandState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShSvn
+{
+    public class CommandState
+    {
+        internal void SetConfigDir(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetPassword(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetUserName(string p)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void SetRevision(string p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
