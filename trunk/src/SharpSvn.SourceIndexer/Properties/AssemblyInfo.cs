caed5415d787734f9fb1bc2abb94c72d9403be0c
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-06-19 15:14:46 +0000

    Updated version number start from 1.4999 to 1.5000. Subversion 1.5.0 is official now

--- trunk/src/SharpSvn.SourceIndexer/Properties/AssemblyInfo.cs
+++ trunk/src/SharpSvn.SourceIndexer/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4999.0.*")]
+[assembly: AssemblyVersion("1.5000.0.*")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
