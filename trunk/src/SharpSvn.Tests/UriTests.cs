22cfb13ec3de0fdc3e99b8a37a5d07dfc485f294
sander <sander@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-06-30 07:44:01 +0000

    Add failing testcase demonstrating problem in Url normalization

---
+++ trunk/src/SharpSvn.Tests/UriTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+using SharpSvn.Implementation;
+
+namespace SharpSvn.Tests
+{
+    [TestFixture]
+    public class UriTests
+    {
+        [Test]
+        public void TestNormalizeUri()
+        {
+            Assert.That(SvnTools.GetNormalizedUri(new Uri("http://localhost/test")).AbsoluteUri, Is.EqualTo("http://localhost/test"));
+            Assert.That(SvnTools.GetNormalizedUri(new Uri("http://localhost/test/")).AbsoluteUri, Is.EqualTo("http://localhost/test"));
+            Assert.That(SvnTools.GetNormalizedUri(new Uri("http://localhost/test//")).AbsoluteUri, Is.EqualTo("http://localhost/test"));
+
+            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///localhost/"), "a%20b")).AbsoluteUri, Is.EqualTo("file:///localhost/a%20b"));
+            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///localhost/"), "a%20b")).AbsoluteUri, Is.EqualTo("file:///localhost/a%20b"));
+
+            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a b")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
+            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a b/")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
+        }
+    }
+}
