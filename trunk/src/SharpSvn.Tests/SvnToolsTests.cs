dcdedd4a5b5f0c05882cc3fcb40a64403bd1ac6d
sander <sander@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-06-30 12:33:18 +0000

    Rename testclass and add PathToUri tests

--- trunk/src/SharpSvn.Tests/SvnToolsTests.cs
+++ trunk/src/SharpSvn.Tests/SvnToolsTests.cs
@@ -8,7 +8,7 @@ using SharpSvn.Implementation;
 namespace SharpSvn.Tests
 {
     [TestFixture]
-    public class UriTests
+    public class SvnToolsTests
     {
         [Test]
         public void TestNormalizeUri()
@@ -28,6 +28,16 @@ namespace SharpSvn.Tests
             Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a%20b")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
             Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a%20b/")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
 
+        }
+
+        [Test]
+        public void TestPathToUri()
+        {
+            Uri root = new Uri("http://server/");
+            Assert.That(new Uri(root, SvnTools.PathToUri("\\a b\\test")).AbsoluteUri, Is.EqualTo("http://server/a%20b/test"));
+            Assert.That(new Uri(root, SvnTools.PathToUri("\\a b\\test\\")).AbsoluteUri, Is.EqualTo("http://server/a%20b/test/"));
+
+            Assert.That(new Uri(root, SvnTools.PathToUri("\\c#\\test\\")).AbsoluteUri, Is.EqualTo("http://server/c%23/test/"));
         }
     }
 }
