1e72263c2022a380d694fb0c4ba8bf0738537d27
Sander Rijken <1292085+srijken@users.noreply.github.com>
2008-06-30 09:20:51 +0000

    Add extra test-url

--- src/SharpSvn.Tests/UriTests.cs
+++ src/SharpSvn.Tests/UriTests.cs
@@ -13,15 +13,21 @@ namespace SharpSvn.Tests
         [Test]
         public void TestNormalizeUri()
         {
+            Assert.That(SvnTools.GetNormalizedUri(new Uri("https://svn.apache.org/repos/asf/incubator/lucene.net/trunk/C%23/")).AbsoluteUri,
+                Is.EqualTo("https://svn.apache.org/repos/asf/incubator/lucene.net/trunk/C%23"));
+
+
             Assert.That(SvnTools.GetNormalizedUri(new Uri("http://localhost/test")).AbsoluteUri, Is.EqualTo("http://localhost/test"));
             Assert.That(SvnTools.GetNormalizedUri(new Uri("http://localhost/test/")).AbsoluteUri, Is.EqualTo("http://localhost/test"));
             Assert.That(SvnTools.GetNormalizedUri(new Uri("http://localhost/test//")).AbsoluteUri, Is.EqualTo("http://localhost/test"));
 
-            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///localhost/"), "a%20b")).AbsoluteUri, Is.EqualTo("file:///localhost/a%20b"));
-            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///localhost/"), "a%20b")).AbsoluteUri, Is.EqualTo("file:///localhost/a%20b"));
 
             Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a b")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
             Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a b/")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
+            
+            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a%20b")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
+            Assert.That(SvnTools.GetNormalizedUri(new Uri(new Uri("file:///c:/"), "a%20b/")).AbsoluteUri, Is.EqualTo("file:///c:/a%20b"));
+
         }
     }
 }
