070153e05fcebae18dec9268a67b2e543a73f53d
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-11-23 14:20:28 +0000

    * src/SharpSvn/Commands/FileVersions.cpp * src/SharpSvn/EventArgs/SvnFileVersionEventArgs.h * src/SharpSvn/Args/FileVersions.h   Implement property handling in the experimental SvnClient.FileVersions   command. Keeping track of file properties is required for file translation
    * src/SharpSvn.Tests/Commands/FileVersions.cs
      Updated test to check property behavior

--- trunk/src/SharpSvn/EventArgs/SvnFileVersionEventArgs.h
+++ trunk/src/SharpSvn/EventArgs/SvnFileVersionEventArgs.h
@@ -17,21 +17,30 @@ namespace SharpSvn {
 		String^ _path;
 
 		initonly DateTime _date;
+		String^ _author;
 		String^ _message;
 
+        SvnPropertyCollection^ _revisionProperties;
 		SvnPropertyCollection^ _properties;
 		AprPool^ _pool;
 
-		String^ _author;
 		String^ _versionFile;
 
 		const char* _pPath;
 		const char* _pcAuthor;
 		const char* _pcMessage;
 		apr_hash_t* _revProps;
+        apr_hash_t* _fileProps;
 
 	internal:
-		SvnFileVersionEventArgs(String^ versionFile, const char *path, svn_revnum_t rev, apr_hash_t *rev_props, bool result_of_merge, AprPool^ pool)
+		SvnFileVersionEventArgs(
+            String^ versionFile, 
+            const char *path, 
+            svn_revnum_t rev, 
+            apr_hash_t *rev_props, 
+            apr_hash_t *file_props,
+            bool result_of_merge, 
+            AprPool^ pool)
 		{
 			if (!path)
 				throw gcnew ArgumentNullException("path");
@@ -44,6 +53,9 @@ namespace SharpSvn {
 
 			_versionFile = versionFile;
 			_pPath = path;
+
+            _revProps = rev_props;
+            _fileProps = file_props;
 
 			const char* pcAuthor = nullptr;
 			const char* pcDate = nullptr;
@@ -70,6 +82,52 @@ namespace SharpSvn {
 		}
 
 	public:
+        property __int64 Revision
+        {
+            __int64 get()
+            {
+                return _revision;
+            }
+        }
+
+		property DateTime Time
+		{
+			DateTime get()
+			{
+				return _date;
+			}
+		}
+
+		property String^ Author
+		{
+			String^ get()
+			{
+				if (!_author && _pcAuthor)
+					_author = SvnBase::Utf8_PtrToString(_pcAuthor);
+
+				return _author;
+			}
+		}
+
+		property String^ LogMessage
+		{
+			String^ get()
+			{
+				if (!_message && _pcMessage)
+				{
+					_message = SvnBase::Utf8_PtrToString(_pcMessage);
+
+					if (_message)
+					{
+						// Subversion log messages always use \n newlines
+						_message = _message->Replace("\n", Environment::NewLine);
+					}
+				}
+
+				return _message;
+			}
+		}
+
 		property String^ Path
 		{
 			String^ get()
@@ -104,6 +162,29 @@ namespace SharpSvn {
 			}
 		}
 
+        property SvnPropertyCollection^ RevisionProperties
+        {
+            SvnPropertyCollection^ get()
+            {
+                if (!_revisionProperties && _revProps && _pool)
+                    _revisionProperties = SvnBase::CreatePropertyDictionary(_revProps, _pool);
+
+                return _revisionProperties;
+            }
+        }
+
+        property SvnPropertyCollection^ Properties
+        {
+            SvnPropertyCollection^ get()
+            {
+                if (!_properties && _fileProps && _pool)
+                    _properties = SvnBase::CreatePropertyDictionary(_fileProps, _pool);
+
+                return _properties;
+            }
+        }
+
+
 	protected public:
 		virtual void Detach(bool keepProperties) override
 		{
@@ -111,9 +192,12 @@ namespace SharpSvn {
 			{
 				if (keepProperties)
 				{
+					GC::KeepAlive(Author);
+					GC::KeepAlive(LogMessage);
 					GC::KeepAlive(Path);
+                    GC::KeepAlive(RevisionProperties);
+                    GC::KeepAlive(Properties);
 				}
-
 			}
 			finally
 			{
@@ -122,6 +206,7 @@ namespace SharpSvn {
 				_pcAuthor = nullptr;
 				_pcMessage = nullptr;
 				_revProps = nullptr;
+                _fileProps = nullptr;
 
 				__super::Detach(keepProperties);
 			}
