872bfc1b29eb556c53dc853301ccddfaffb6cd98
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2008-12-27 15:19:16 +0000

    Add support for automatically removing old uploaded daily builds from collabnet

--- trunk/src/tools/CollabUpload/Program.cs
+++ trunk/src/tools/CollabUpload/Program.cs
@@ -7,6 +7,7 @@ using System.Xml;
 using System.Web;
 using QQn.TurtleUtils.IO;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace CollabUpload
 {
@@ -24,8 +25,23 @@ namespace CollabUpload
             public string Description;
 
             public string Result;
+            public int MaxUploads;
+            public TimeSpan Keep;
 
             public readonly List<string> Files = new List<string>();
+        }
+
+        class Document
+        {
+            public Uri DownloadUri;
+            public string Name;
+            public string Who;
+            public string Status;
+            public DateTime Date;
+            public Uri ReserveUri;
+            public string Description;
+            public Uri EditUri;
+            public int FileId;
         }
 
         static void Main(string[] args)
@@ -103,6 +119,16 @@ namespace CollabUpload
                             aa.Result = value;
                             i++;
                             break;
+                        case "--max-uploads":
+                            if (!argAvailable || !int.TryParse(value, out aa.MaxUploads))
+                                return ArgError("--max-uploads requires integer argument");
+                            i++;
+                            break;
+                        case "--keep":
+                            if (!argAvailable || !TimeSpan.TryParse(value, out aa.Keep))
+                                return ArgError("--keep requires timespan argument DD.hh:mm");
+                            i++;
+                            break;
                         default:
                             return ArgError(string.Format("Unknown argument: {0}", value));
                     }
@@ -123,13 +149,15 @@ namespace CollabUpload
             return null;
         }
 
+        const string UserAgentName = "Mozilla/8.0 (compatible; Collab Uploader C#)";
+
         static Args Run(Args args)
         {
             CookieContainer cookBox = new CookieContainer();
 
             string requestUri = string.Format("http://{0}/servlets/ProjectDocumentAdd?folderID={1}", args.Site, args.Folder);
             HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(requestUri);
-            wr.UserAgent = "Mozilla/8.0 (compatible; Collab Uploader C#)";
+            wr.UserAgent = UserAgentName;
             wr.CookieContainer = cookBox;
             string responseUri;
             string text;
@@ -140,62 +168,7 @@ namespace CollabUpload
                 text = GetText(response);
             }
 
-            if (responseUri != requestUri && responseUri.Contains("/Login"))
-            {
-                // Standard Collabnet login on secondary host..
-                XmlDocument doc = new XmlDocument();
-
-                XmlReaderSettings xs = new XmlReaderSettings();
-                xs.ValidationFlags = System.Xml.Schema.XmlSchemaValidationFlags.None;
-                xs.ValidationType = ValidationType.None;
-                xs.ProhibitDtd = false;
-
-                int nS = text.IndexOf("<html");
-
-                if (nS > 0)
-                    text = text.Substring(nS).Replace("&nbsp;","&#160;").Replace("&copy;", "&#169;");
-
-                doc.Load(XmlReader.Create(new StringReader(text), xs));
-
-                XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
-                nsmgr.AddNamespace("html", "http://www.w3.org/1999/xhtml");
-                XmlElement form = (XmlElement)doc.SelectSingleNode("//html:form[@id='loginform']", nsmgr);
-                StringBuilder sb = new StringBuilder();
-
-                wr = (HttpWebRequest)WebRequest.Create(new Uri(new Uri(responseUri), new Uri(form.GetAttribute("action"))));
-                wr.UserAgent = "Mozilla/8.0 (compatible; Collab Uploader C#)";
-                wr.CookieContainer = cookBox;
-                using(WebFormWriter wfw = new WebFormWriter(wr, WebRequestPostDataEncoding.WwwFormUrlEncoded, "POST"))
-                {
-                    foreach (XmlElement el in form.SelectNodes(".//html:input[@name]", nsmgr))
-                    {
-                        string name = el.GetAttribute("name");
-
-                        switch (name.ToUpperInvariant())
-                        {
-                            case "LOGINID":
-                                wfw.AddValue(name, args.UserName);
-                                break;
-                            case "PASSWORD":
-                                wfw.AddValue(name, args.Password);
-                                break;
-                            default:
-                                string v = el.GetAttribute("value");
-                                wfw.AddValue(name, v ?? "");
-                                break;
-                        }
-                    }
-                }
-
-                using (WebResponse response = wr.GetResponse())
-                {
-                    responseUri = response.ResponseUri.ToString();
-                    text = GetText(response);
-                }
-
-                if (responseUri != requestUri)
-                    return ArgError("Invalid credentials specified");
-            }
+            MaybeLogin(args, cookBox, requestUri, responseUri, ref text);
 
             // Ok, we are logged in; let's upload the files
 
@@ -206,7 +179,7 @@ namespace CollabUpload
             {
                 Console.WriteLine("Uploading {0}", Path.GetFileName(file));
                 wr = (HttpWebRequest)WebRequest.Create(requestUri + "&action=Add%20document");
-                wr.UserAgent = "Mozilla/8.0 (compatible; Collab Uploader C#)";
+                wr.UserAgent = UserAgentName;
                 wr.CookieContainer = cookBox;
                 wr.Timeout = 1800 * 1000; // Half an hour should be enough 
                 using (WebFormWriter wfw = new WebFormWriter(wr, WebRequestPostDataEncoding.MultipartFormData, "POST"))
@@ -246,7 +219,225 @@ namespace CollabUpload
                     }
                 }
             }
+
+            if (args.MaxUploads > 0 || args.Keep != TimeSpan.Zero)
+            {
+                List<Document> docs = GetDocumentList(args, cookBox);
+
+                RemoveOnStatus(args, docs); // Filter all not interesting documents (status, locks)
+                SortDocs(docs);
+
+                if (args.MaxUploads > 0 && docs.Count > args.MaxUploads)
+                {
+                    int from = docs.Count - args.MaxUploads;
+                    docs.RemoveRange(from, docs.Count - from);
+                }
+
+                if (args.Keep > TimeSpan.Zero)
+                {
+                    FilterOnDate(docs, DateTime.Now.Date - args.Keep);
+                }                
+
+                GC.KeepAlive(docs);
+                foreach (Document doc in docs)
+                {
+                    Console.WriteLine("Deleting {0}", doc.Name);
+
+                    DeleteFile(args, cookBox, doc.FileId);
+                }
+            }
             return null;
+        }
+
+        private static void RemoveOnStatus(Args args, List<Document> docs)
+        {
+            string filterStatus = args.Status ?? "Draft";
+            docs.RemoveAll(delegate(Document d)
+            {
+                if (d.ReserveUri == null)
+                    return true; // Document is reserved                    
+                else if (!string.Equals(filterStatus, d.Status, StringComparison.OrdinalIgnoreCase))
+                    return true; // Status doesn't match
+
+                return false;
+            });                
+        }
+
+        private static void SortDocs(List<Document> docs)
+        {
+            docs.Sort(
+                delegate(Document d1, Document d2)
+                {
+                    return StringComparer.OrdinalIgnoreCase.Compare(d1.Name, d2.Name);
+                });
+        }
+
+        private static void FilterOnDate(List<Document> docs, DateTime keepAfter)
+        {
+            docs.RemoveAll(
+                delegate(Document d)
+                {
+                    if (d.Date != DateTime.MinValue && d.Date >= keepAfter)
+                        return true;
+                    else
+                        return false;
+                });
+        }
+
+        private static void MaybeLogin(Args args, CookieContainer cookBox, string requestUri, string responseUri, ref string text)
+        {
+            HttpWebRequest wr;
+            if (responseUri != requestUri && responseUri.Contains("/Login"))
+            {
+                // Standard Collabnet login on secondary host..
+                XmlDocument doc = new XmlDocument();
+
+                XmlReaderSettings xs = new XmlReaderSettings();
+                xs.ValidationFlags = System.Xml.Schema.XmlSchemaValidationFlags.None;
+                xs.ValidationType = ValidationType.None;
+                xs.ProhibitDtd = false;
+
+                int nS = text.IndexOf("<html");
+
+                if (nS > 0)
+                    text = text.Substring(nS).Replace("&nbsp;", "&#160;").Replace("&copy;", "&#169;");
+
+                doc.Load(XmlReader.Create(new StringReader(text), xs));
+
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+                nsmgr.AddNamespace("html", "http://www.w3.org/1999/xhtml");
+                XmlElement form = (XmlElement)doc.SelectSingleNode("//html:form[@id='loginform']", nsmgr);
+                StringBuilder sb = new StringBuilder();
+
+                wr = (HttpWebRequest)WebRequest.Create(new Uri(new Uri(responseUri), new Uri(form.GetAttribute("action"))));
+                wr.UserAgent = UserAgentName;
+                wr.CookieContainer = cookBox;
+                using (WebFormWriter wfw = new WebFormWriter(wr, WebRequestPostDataEncoding.WwwFormUrlEncoded, "POST"))
+                {
+                    foreach (XmlElement el in form.SelectNodes(".//html:input[@name]", nsmgr))
+                    {
+                        string name = el.GetAttribute("name");
+
+                        switch (name.ToUpperInvariant())
+                        {
+                            case "LOGINID":
+                                wfw.AddValue(name, args.UserName);
+                                break;
+                            case "PASSWORD":
+                                wfw.AddValue(name, args.Password);
+                                break;
+                            default:
+                                string v = el.GetAttribute("value");
+                                wfw.AddValue(name, v ?? "");
+                                break;
+                        }
+                    }
+                }
+
+                using (WebResponse response = wr.GetResponse())
+                {
+                    responseUri = response.ResponseUri.ToString();
+                    text = GetText(response);
+                }
+
+                if (responseUri != requestUri)
+                {
+                    throw new InvalidOperationException("Invalid credentials specified");
+                }
+            }
+        }
+
+        private static List<Document> GetDocumentList(Args args, CookieContainer cookBox)
+        {
+            HttpWebRequest wr;
+            string responseUri;
+            string text;
+
+            string requestUri = string.Format("http://{0}/servlets/ProjectDocumentList?folderID={1}", args.Site, args.Folder);
+
+            wr = (HttpWebRequest)WebRequest.Create(requestUri);
+            wr.UserAgent = "Mozilla/8.0 (compatible; Collab Uploader C#)";
+            wr.CookieContainer = cookBox;
+
+            using (WebResponse response = wr.GetResponse())
+            {
+                responseUri = response.ResponseUri.ToString();
+                text = GetText(response);
+            }
+
+            string body = "([^<]|(\\<(a|div)[^>]*>[^<]*\\<\\/(a|div)>))*";
+
+            Regex re = new Regex("\\<tr[^>]*>\\s*\\<td[^>]*>\\s*\\<div[^>]+class=\"leaf\"[^>]*\\>\\s*" +
+                "\\<a href=\"(?<url>[^\"]*)\"[^>]*>\\s*(?<name>[^<]*)\\<\\/a\\>\\s*\\<\\/div\\>\\s*\\<\\/td>\\s*" +
+                "\\<td[^>]*>\\s*(?<status>[^<]*)\\</td>\\s*" +
+                "\\<td[^>]*>\\s*\\<(a)[^>]*>(?<who>[^<]*)<\\/a>\\s*on\\s(?<date>[^<]*)\\<\\/td>\\s*" +
+                "\\<td[^>]*>" + body + "\\<\\/td>\\s*" +
+                "\\<td[^>]*>\\s*[^<]*(\\<a[^>]*href=\"(?<reserveUrl>[^\"]*)\"[^>]*>\\s*Reserve[^<]*\\<\\/a>)?" + body + "\\<\\/td>\\s*" +
+                "\\<td[^>]*>\\s*(?<desc>[^<]*)\\<\\/td>\\s*" +
+                "\\<td[^>]*>\\s*\\<a[^>]*href=\"(?<editUrl>[^\"]*)\"[^>]*>\\s*Edit\\s*\\<\\/a>\\s*<\\/td>\\s*" +                
+                "\\<\\/tr\\>"
+                , RegexOptions.Singleline | RegexOptions.ExplicitCapture | RegexOptions.IgnoreCase);
+
+            List<Document> docs = new List<Document>();
+
+            Uri baseUri = new Uri(string.Format("http://{0}/servlets/qq", args.Site));
+            foreach (Match m in re.Matches(text))
+            {
+                string url = m.Groups["url"].Value.Trim();
+                string name = m.Groups["name"].Value.Trim();
+                string status = m.Groups["status"].Value.Trim();
+                string who = m.Groups["who"].Value.Trim();
+                string date = m.Groups["date"].Value.Trim();
+                string reserveUrl = m.Groups["reserveUrl"].Value.Trim();
+                string desc = m.Groups["desc"].Value.Trim();
+                string editUrl = m.Groups["editUrl"].Value.Trim();
+
+                Document d = new Document();
+                d.DownloadUri = new Uri(baseUri, url);
+                d.Name = name;
+                d.Who = who;
+                d.Status = status;
+                d.ReserveUri = string.IsNullOrEmpty(reserveUrl) ? null : new Uri(baseUri, reserveUrl);
+                d.Description = desc;
+                d.EditUri = new Uri(baseUri, editUrl);
+                d.FileId = int.Parse(editUrl.Substring(editUrl.IndexOf("documentID=") + 11));
+
+                DateTime when;
+                if (!string.IsNullOrEmpty(date) && DateTime.TryParse(date.Replace(" at "," "), out when))
+                {
+                    d.Date = when;
+                }
+
+                docs.Add(d);
+            }
+
+            return docs;
+        }
+
+        private static void DeleteFile(Args args, CookieContainer cookBox, int fileId)
+        {
+            string responseUri;
+            string text;
+            string requestUri = string.Format("http://{0}/servlets/ProjectDocumentDelete", args.Site);
+
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(requestUri);
+            wr.UserAgent = UserAgentName;
+            wr.CookieContainer = cookBox;
+            using (WebFormWriter wfw = new WebFormWriter(wr, WebRequestPostDataEncoding.WwwFormUrlEncoded, "POST"))
+            {
+                wfw.AddValue("documentID", fileId.ToString(CultureInfo.InvariantCulture));
+                wfw.AddValue("Button", "Confirm delete");
+                wfw.AddValue("maxDepth", "");
+            }
+
+            using (WebResponse response = wr.GetResponse())
+            {
+                responseUri = response.ResponseUri.ToString();
+
+                if (!response.ResponseUri.AbsolutePath.EndsWith("/ProjectDocumentList", StringComparison.OrdinalIgnoreCase))
+                    throw new InvalidOperationException("Delete failed");
+            }
         }
 
         static string GetText(WebResponse response)
