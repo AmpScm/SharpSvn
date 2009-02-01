1b3d274e676289478a41a6be341bda12f402b201
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2009-02-01 22:15:14 +0000

    * Merged r1036,r1050 (where applicable for 1.5)

---
+++ branches/1.500x/src/SharpSvn.Tests/StaticVerifications.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+
+namespace SharpSvn.Tests
+{
+    [TestFixture]
+    public class StaticVerifications
+    {
+        [Test]
+        public void VerifyEnums()
+        {
+            Assembly asm = typeof(SvnClient).Assembly;
+
+            foreach (Type tp in asm.GetTypes())
+            {
+                if (!tp.IsEnum || !tp.IsPublic)
+                    continue;
+
+                if (tp.GetCustomAttributes(typeof(FlagsAttribute), false).Length == 1 || Enum.GetUnderlyingType(tp) != typeof(int))
+                    continue;
+
+                Assert.That(Enum.IsDefined(tp, 0), "0 value is defined in {0}", tp);                
+
+                int min = 0;
+                int max = 0;
+
+                foreach (int v in Enum.GetValues(tp))
+                {
+                    if (v < min)
+                        min = v;
+
+                    if (v > max)
+                        max = v;
+                }
+
+                if (tp == typeof(SvnCommandType))
+                    max = (int)SvnCommandType.Write;
+                else if (tp == typeof(SvnChangeAction))
+                    continue;
+                else if (tp == typeof(SvnErrorCode))
+                    continue;
+
+                for (int i = min; i <= max; i++)
+                {
+                    Assert.That(Enum.IsDefined(tp, i), "{0} value is defined in {1}", i, tp);
+                }
+            }
+        }
+    }
+}
