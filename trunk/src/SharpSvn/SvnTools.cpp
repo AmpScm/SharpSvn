1d7cdf6254b32242b2b5e14991fac406d95cf4e3
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2009-03-08 22:19:26 +0000

    SvnTools.GetNormalizedFullPath(): Automatically convert \\?\ style paths to normal style and check for paths containing ':' after position 1.
    * src/SharpSvn/SharpSvnStrings.h
    * src/SharpSvn/SharpSvnStrings.resx
      Add error string resource
    * src/SharpSvn/SvnTools.cpp
      Check for 2 styles of invalid paths

--- trunk/src/SharpSvn/SvnTools.cpp
+++ trunk/src/SharpSvn/SvnTools.cpp
@@ -360,10 +360,13 @@ String^ SvnTools::GetNormalizedFullPath(String^ path)
 	if (String::IsNullOrEmpty(path))
 		throw gcnew ArgumentNullException("path");
 
-	if (PathContainsInvalidChars(path))
-		throw gcnew ArgumentException("Path contains invalid characters", "path");
+	if (path->StartsWith("\\\\?\\", StringComparison::Ordinal))
+		path = GetNormalizedFullPath(path->Substring(4));
+
+	if (PathContainsInvalidChars(path) || path->LastIndexOf(':') >= 2)
+		throw gcnew ArgumentException(String::Format(SharpSvnStrings::PathXContainsInvalidCharacters, path), "path");
 	else if (IsNormalizedFullPath(path))
-		return path; // Just pass through; no allocations
+		return path; // Just pass through; no allocations	
 
 	bool retry = true;
 
@@ -375,6 +378,10 @@ String^ SvnTools::GetNormalizedFullPath(String^ path)
 			retry = false;
 		}
 		catch(PathTooLongException^) // Path grew by getting full path
+		{
+			// Use the retry
+		}
+		catch(NotSupportedException^) // Something fishy is gowing on
 		{
 			// Use the retry
 		}
