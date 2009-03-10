b9e210a6101e7afd2b6feb897cd1cbb0114c3486
Bert Huijben <rhuijben@users.noreply.github.com>
2009-03-10 01:35:24 +0000

    * src/SharpSvn.Tests/ExceptionTests.cs * src/SharpSvn.Tests/SharpSvn.Tests-VS2008.csproj   Add exception test

---
+++ src/SharpSvn.Tests/ExceptionTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharpSvn.Tests.Commands;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace SharpSvn.Tests
+{ 
+    [TestFixture]
+    public class ExceptionTests :TestBase
+    {
+        [Test]
+        public void StatusCrash()
+        {
+            string dir = GetTempDir();
+            Client.CheckOut(new Uri(CollabReposUri, "trunk"), dir);
+
+            SvnStatusArgs sa = new SvnStatusArgs();
+            sa.ThrowOnError = false;
+            sa.RetrieveAllEntries = true;
+            Assert.That(Client.Status(dir, sa,
+                delegate(object sender, SvnStatusEventArgs e)
+                {
+                    throw new InvalidOperationException();
+                }), Is.False);
+
+            Assert.That(sa.LastException.RootCause, Is.InstanceOfType(typeof(InvalidOperationException)));
+        }   
+    }
+}
