8672e3dd6ded22bdee82d19049c89eb036abe6c5
Bert Huijben <rhuijben@users.noreply.github.com>
2009-06-02 10:59:23 +0000

    * src/tools/Po2Resx/Po2Resx.csproj * src/tools/Po2Resx/Po2Resx.snk   Add strong name to project * src/SharpSvn.Tests/Commands/AddTests.cs   Extend basic add test

--- src/SharpSvn.Tests/Commands/AddTests.cs
+++ src/SharpSvn.Tests/Commands/AddTests.cs
@@ -23,62 +23,78 @@ using System.Text.RegularExpressions;
 using NUnit.Framework;
 using NUnit.Framework.SyntaxHelpers;
 using SharpSvn;
+using System.Collections.ObjectModel;
 
 namespace SharpSvn.Tests.Commands
 {
-	/// <summary>
-	/// Tests NSvn::Core::Client::Add
-	/// </summary>
-	[TestFixture]
-	public class AddTests : TestBase
-	{
-		/// <summary>
-		/// Attempts to add a file, checking that the file actually was added
-		/// </summary>
-		[Test]
-		public void TestBasic()
-		{
-			string testFile = this.CreateTextFile("testfile.txt");
+    /// <summary>
+    /// Tests NSvn::Core::Client::Add
+    /// </summary>
+    [TestFixture]
+    public class AddTests : TestBase
+    {
+        /// <summary>
+        /// Attempts to add a file, checking that the file actually was added
+        /// </summary>
+        [Test]
+        public void TestBasic()
+        {
+            string dir = GetTempDir();
+            string testFile = Path.Combine(dir, "testfile.txt");
 
-			this.Client.Notify += new EventHandler<SvnNotifyEventArgs>(this.NotifyCallback);
-			SvnAddArgs a = new SvnAddArgs();
-			a.Depth = SvnDepth.Empty;
-			this.Client.Add(testFile, a);
+            using (SvnClient client = new SvnClient())
+            {
+                client.CheckOut(ReposUrl, dir);
 
-            Guid g;
-            Assert.That(Client.TryGetRepositoryId(testFile, out g));
-            Assert.That(g, Is.Not.EqualTo(Guid.Empty));
+                File.WriteAllText(testFile, "This is a testfile");
 
-			Assert.That(this.Notifications.Length > 0, "No notification callbacks received");
+                SvnAddArgs a = new SvnAddArgs();
+                a.Depth = SvnDepth.Empty;
 
-			Assert.That(this.GetSvnStatus(testFile), Is.EqualTo('A'), "svn st does not report the file as added");
+                client.Add(testFile, a);
 
-            Assert.That(Client.Commit(WcPath));
-		}
+                // Check if the file is part of a working copy
+                Guid g;
+                Assert.That(client.TryGetRepositoryId(testFile, out g));
+                Assert.That(g, Is.Not.EqualTo(Guid.Empty));
 
-		/// <summary>
-		/// Creates a subdirectory with items in it, tries to add it non-recursively.
-		/// Checks that none of the subitems are added
-		/// </summary>
-		[Test]
-		public void TestAddDirectoryNonRecursively()
-		{
-			string dir1, dir2, testFile1, testFile2;
-			this.CreateSubdirectories(out dir1, out dir2, out testFile1, out testFile2);
+                Collection<SvnStatusEventArgs> st;
+                client.GetStatus(testFile, out st);
 
-			this.Client.Notify += new EventHandler<SvnNotifyEventArgs>(this.NotifyCallback);
-			SvnAddArgs a = new SvnAddArgs();
-			a.Depth = SvnDepth.Empty;
-			// do a non-recursive add here
-			this.Client.Add(dir1, a);
+                Assert.That(st.Count, Is.EqualTo(1));
+                Assert.That(st[0].LocalContentStatus, Is.EqualTo(SvnStatus.Added));
 
-			Assert.That(this.Notifications.Length == 1, "Too many or no notifications received. Added recursively?");
-			Assert.That(this.GetSvnStatus(dir1), Is.EqualTo('A'), "Subdirectory not added");
+                // Verify status with external command
+                Assert.That(this.GetSvnStatus(testFile), Is.EqualTo('A'), "svn st does not report the file as added");
 
-			Assert.That(GetSvnStatus(dir2), Is.Not.EqualTo('A'), "Recursive add");
-			Assert.That(GetSvnStatus(testFile1), Is.Not.EqualTo('A'), "Recursive add");
-			Assert.That(GetSvnStatus(testFile2), Is.Not.EqualTo('A'), "Recursive add");
-		}
+                client.Configuration.LogMessageRequired = false;
+                Assert.That(client.Commit(dir));
+            }
+        }
+
+        /// <summary>
+        /// Creates a subdirectory with items in it, tries to add it non-recursively.
+        /// Checks that none of the subitems are added
+        /// </summary>
+        [Test]
+        public void TestAddDirectoryNonRecursively()
+        {
+            string dir1, dir2, testFile1, testFile2;
+            this.CreateSubdirectories(out dir1, out dir2, out testFile1, out testFile2);
+
+            this.Client.Notify += new EventHandler<SvnNotifyEventArgs>(this.NotifyCallback);
+            SvnAddArgs a = new SvnAddArgs();
+            a.Depth = SvnDepth.Empty;
+            // do a non-recursive add here
+            this.Client.Add(dir1, a);
+
+            Assert.That(this.Notifications.Length == 1, "Too many or no notifications received. Added recursively?");
+            Assert.That(this.GetSvnStatus(dir1), Is.EqualTo('A'), "Subdirectory not added");
+
+            Assert.That(GetSvnStatus(dir2), Is.Not.EqualTo('A'), "Recursive add");
+            Assert.That(GetSvnStatus(testFile1), Is.Not.EqualTo('A'), "Recursive add");
+            Assert.That(GetSvnStatus(testFile2), Is.Not.EqualTo('A'), "Recursive add");
+        }
 
         [Test]
         public void TestAddWithParents()
@@ -114,61 +130,61 @@ namespace SharpSvn.Tests.Commands
 
         }
 
-		/// <summary>
-		/// Creates a subdirectory with some items in it. Attempts to add it recursively.
-		/// </summary>
-		[Test]
-		public void TestAddDirectoryRecursively()
-		{
-			string dir1, dir2, testFile1, testFile2;
-			this.CreateSubdirectories(out dir1, out dir2, out testFile1, out testFile2);
+        /// <summary>
+        /// Creates a subdirectory with some items in it. Attempts to add it recursively.
+        /// </summary>
+        [Test]
+        public void TestAddDirectoryRecursively()
+        {
+            string dir1, dir2, testFile1, testFile2;
+            this.CreateSubdirectories(out dir1, out dir2, out testFile1, out testFile2);
 
-			this.Client.Notify += new EventHandler<SvnNotifyEventArgs>(this.NotifyCallback);
-			// now a recursive add
-			SvnAddArgs a = new SvnAddArgs();
-			a.Depth = SvnDepth.Infinity;
-			this.Client.Add(dir1, a);
+            this.Client.Notify += new EventHandler<SvnNotifyEventArgs>(this.NotifyCallback);
+            // now a recursive add
+            SvnAddArgs a = new SvnAddArgs();
+            a.Depth = SvnDepth.Infinity;
+            this.Client.Add(dir1, a);
 
-			// enough notifications?
-			Assert.That(this.Notifications.Length, Is.EqualTo(4), "Received wrong number of notifications");
-			Assert.That(this.GetSvnStatus(dir1), Is.EqualTo('A'), "Subdirectory not added");
-			Assert.That(this.GetSvnStatus(dir2), Is.EqualTo('A'), "Subsubdirectory not added");
-			Assert.That(this.GetSvnStatus(testFile1), Is.EqualTo('A'), "File in subdirectory not added");
-			Assert.That(this.GetSvnStatus(testFile2), Is.EqualTo('A'), "File in subsubdirectory not added");
-		}
+            // enough notifications?
+            Assert.That(this.Notifications.Length, Is.EqualTo(4), "Received wrong number of notifications");
+            Assert.That(this.GetSvnStatus(dir1), Is.EqualTo('A'), "Subdirectory not added");
+            Assert.That(this.GetSvnStatus(dir2), Is.EqualTo('A'), "Subsubdirectory not added");
+            Assert.That(this.GetSvnStatus(testFile1), Is.EqualTo('A'), "File in subdirectory not added");
+            Assert.That(this.GetSvnStatus(testFile2), Is.EqualTo('A'), "File in subsubdirectory not added");
+        }
 
-		[Test]
-		public void TestWithForce()
-		{
-			SvnAddArgs a = new SvnAddArgs();
-			a.Depth = SvnDepth.Empty;
+        [Test]
+        public void TestWithForce()
+        {
+            SvnAddArgs a = new SvnAddArgs();
+            a.Depth = SvnDepth.Empty;
 
-			string file = Path.Combine(this.WcPath, "AssemblyInfo.cs");
-			try
-			{
-				this.Client.Add(file, a);
-				Assert.Fail("Should have failed");
-			}
-			catch (SvnException)
-			{
-				// swallow
-			}
+            string file = Path.Combine(this.WcPath, "AssemblyInfo.cs");
+            try
+            {
+                this.Client.Add(file, a);
+                Assert.Fail("Should have failed");
+            }
+            catch (SvnException)
+            {
+                // swallow
+            }
 
-			// should not fail
-			a.Force = true;
-			this.Client.Add(file, a);
-		}
+            // should not fail
+            a.Force = true;
+            this.Client.Add(file, a);
+        }
 
-		[Test]
-		public void TestAddFileWithNonAsciiFilename()
-		{
-			SvnAddArgs a = new SvnAddArgs();
-			a.Depth = SvnDepth.Infinity;
+        [Test]
+        public void TestAddFileWithNonAsciiFilename()
+        {
+            SvnAddArgs a = new SvnAddArgs();
+            a.Depth = SvnDepth.Infinity;
 
-			string newFile = Path.Combine(this.WcPath, "eia.");
-			File.Create(newFile).Close();
-			this.Client.Add(newFile, a);
-		}
+            string newFile = Path.Combine(this.WcPath, "eia.");
+            File.Create(newFile).Close();
+            this.Client.Add(newFile, a);
+        }
 
         [Test]
         public void TestMultiLevel()
@@ -214,7 +230,7 @@ namespace SharpSvn.Tests.Commands
             TouchFile(file);
             Client.Add(file);
 
-            Client.Commit(WcPath);            
+            Client.Commit(WcPath);
 
             File.Delete(file);
             Assert.That(!File.Exists(file));
@@ -238,25 +254,25 @@ namespace SharpSvn.Tests.Commands
             da.ThrowOnError = true; // This works in 1.5
             da.KeepLocal = true;
             Client.Delete(file, da);
-            
-            Client.Status(dir, 
+
+            Client.Status(dir,
                 delegate(object sender, SvnStatusEventArgs e)
                 {
                     Assert.That(false, "Nothing modified");
                 });
         }
 
-		private void CreateSubdirectories(out string dir1, out string dir2, out string testFile1, out string testFile2)
-		{
-			dir1 = Path.Combine(this.WcPath, "subdir");
-			Directory.CreateDirectory(dir1);
+        private void CreateSubdirectories(out string dir1, out string dir2, out string testFile1, out string testFile2)
+        {
+            dir1 = Path.Combine(this.WcPath, "subdir");
+            Directory.CreateDirectory(dir1);
 
-			dir2 = Path.Combine(dir1, "subsubdir");
-			Directory.CreateDirectory(dir2);
+            dir2 = Path.Combine(dir1, "subsubdir");
+            Directory.CreateDirectory(dir2);
 
-			testFile1 = this.CreateTextFile(@"subdir\testfile.txt");
-			testFile2 = this.CreateTextFile(@"subdir\subsubdir\testfile2.txt");
-		}
+            testFile1 = this.CreateTextFile(@"subdir\testfile.txt");
+            testFile2 = this.CreateTextFile(@"subdir\subsubdir\testfile2.txt");
+        }
 
-	}
+    }
 }
