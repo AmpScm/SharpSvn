8042712f77e2e92e99ad2fec7bb2251554f98751
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2009-06-03 11:55:46 +0000

    Merged r1252,r1253 and r1260 from trunk (and resolved a few minor conflicts)

---
+++ branches/1.500x/src/SharpSvn/LookArgs/SvnLookPropertyListArgs.h
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace SharpSvn {
+
+	/// <summary>Extended Parameter container of <see cref="SvnLookClient" />'s Changed method</summary>
+	/// <threadsafety static="true" instance="false"/>
+	public ref class SvnLookPropertyListArgs : public SvnClientArgs
+	{
+	public:
+		SvnLookPropertyListArgs()
+		{
+		}
+
+		virtual property SvnCommandType CommandType
+		{
+			virtual SvnCommandType get() override sealed
+			{
+				return SvnCommandType::Unknown;
+			}
+		}	
+	};
+}
