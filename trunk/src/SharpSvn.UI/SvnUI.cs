39dfd7773479e9c172e3f4f7d5f6a5832a8e3c10
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2010-01-21 01:06:12 +0000

    Allow hooking a SvnRemoteSession to the common UI.
    * src/SharpSvn.UI/SvnClientUIHandler.cs
    * src/SharpSvn.UI/SvnUI.cs
      Add bindings

--- trunk/src/SharpSvn.UI/SvnUI.cs
+++ trunk/src/SharpSvn.UI/SvnUI.cs
@@ -47,8 +47,41 @@ namespace SharpSvn.UI
 		/// Binds the specified client to the user interface defined by args
 		/// </summary>
 		/// <param name="client">The client to bind.</param>
-        /// <param name="parentWindow">The parent window.</param>
+		/// <param name="parentWindow">The parent window.</param>
 		public static void Bind(SvnClient client, IWin32Window parentWindow)
+		{
+			if (client == null)
+				throw new ArgumentNullException("client");
+
+			SvnUIBindArgs args = new SvnUIBindArgs();
+			args.ParentWindow = parentWindow;
+
+			Bind(client, args);
+		}
+
+		/// <summary>
+		/// Binds the specified client to the user interface defined by args
+		/// </summary>
+		/// <param name="client">The client to bind.</param>
+		/// <param name="args">The args.</param>
+		public static void Bind(SvnRemoteSession client, SvnUIBindArgs args)
+		{
+			if (client == null)
+				throw new ArgumentNullException("client");
+			else if (args == null)
+				throw new ArgumentNullException("args");
+
+			SvnClientUIHandler handler = new SvnClientUIHandler(args.ParentWindow);
+
+			handler.Bind(client, args);
+		}
+
+		/// <summary>
+		/// Binds the specified client to the user interface defined by args
+		/// </summary>
+		/// <param name="client">The client to bind.</param>
+		/// <param name="parentWindow">The parent window.</param>
+		public static void Bind(SvnRemoteSession client, IWin32Window parentWindow)
 		{
 			if (client == null)
 				throw new ArgumentNullException("client");
