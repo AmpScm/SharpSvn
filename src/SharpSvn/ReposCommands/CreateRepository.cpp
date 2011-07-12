c82f7b8023f01297a18675edfe644c66c95f5e80
Bert Huijben <rhuijben@users.noreply.github.com>
2011-07-12 12:36:35 +0000

    Adjust for removing of the Subversion 1.7 repository format.

--- src/SharpSvn/ReposCommands/CreateRepository.cpp
+++ src/SharpSvn/ReposCommands/CreateRepository.cpp
@@ -110,16 +110,9 @@ bool SvnRepositoryClient::CreateRepository(String^ repositoryPath, SvnCreateRepo
 			"1");
 		// fall through
 	case SvnRepositoryCompatibility::Subversion16:
-		// Use 1.6 format
-		apr_hash_set(
-			fs_config,
-			SVN_FS_CONFIG_PRE_1_7_COMPATIBLE,
-			APR_HASH_KEY_STRING,
-			"1");
 		// fall through
 	case SvnRepositoryCompatibility::Subversion17:
 		// .....
-
 		break;
 	case SvnRepositoryCompatibility::Default:
 	default:
