3c47511ecc0c7ad0cd6225b9e316a6bb156bc5a0
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2012-07-17 21:55:44 +0000

    Script tweak

--- trunk/src/Copy-Dependencies.cmd
+++ trunk/src/Copy-Dependencies.cmd
@@ -3,7 +3,7 @@ set TO=%1
 set CONFIGURATION=%2
 set PLATFORM=%3
 set SUFFIX=%4
-xcopy /r /y /q /i "..\..\imports\release\bin\*-%PLATFORM%.dll" %TO% || echo Copying dependencies failed
+xcopy /r /y /q /i "..\..\imports\release\bin\*-%PLATFORM%*dll" %TO% || echo Copying dependencies failed
 copy "..\SharpPlink\bin%SUFFIX%\%PLATFORM%\%CONFIGURATION%\SharpPlink-%PLATFORM%.exe" %TO%\SharpPlink-%PLATFORM%.svnExe || echo Copying SharpPlink failed
 
 exit 0
