be4fbd51985517bba95a9bf4a0e29b116ab42352
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2013-05-06 11:20:13 +0000

    Extend branch and reference support.

---
+++ trunk/src/SharpGit/Plumbing/GitBranch.h
@@ -0,0 +1,125 @@
+#pragma once
+#include "GitClientContext.h"
+
+namespace SharpGit {
+	namespace Plumbing {
+		ref class GitReference;
+		public ref class GitBranch : public Implementation::GitBase
+		{
+			initonly GitRepository ^_repository;
+			initonly String ^_name;
+			initonly int _type;
+			GitReference^ _reference;
+
+		internal:
+			GitBranch(GitRepository^ repository, String ^name, git_branch_t type)
+			{
+				if (! repository)
+					throw gcnew ArgumentNullException("repository");
+				else if (String::IsNullOrEmpty(name))
+					throw gcnew ArgumentNullException("name");
+
+				_repository = repository;
+				_name = name;
+				_type = type;
+			}
+
+			GitBranch(GitRepository^ repository, String ^name, GitReference^ reference)
+			{
+				if (! repository)
+					throw gcnew ArgumentNullException("repository");
+				else if (String::IsNullOrEmpty(name))
+					throw gcnew ArgumentNullException("name");
+				else if (! reference)
+					throw gcnew ArgumentNullException("reference");
+
+				_repository = repository;
+				_name = name;
+				_reference = reference;
+				_type = GIT_BRANCH_LOCAL;
+			}
+
+		public:
+			property String^ Name
+			{
+				String^ get()
+				{
+					return _name;
+				}
+			}
+
+			property GitReference^ Reference
+			{
+				GitReference^ get();
+			}
+
+			virtual String^ ToString() override
+			{
+				return Name;
+			}
+
+			property bool IsLocal
+			{
+				bool get()
+				{
+					return 0 != (_type & GIT_BRANCH_LOCAL);
+				}
+			}
+
+			property bool IsRemote
+			{
+				bool get()
+				{
+					return 0 != (_type & GIT_BRANCH_REMOTE);
+				}
+			}
+		};
+
+
+		public ref class GitBranchCollection sealed : public Implementation::GitBase,
+											   public System::Collections::Generic::IEnumerable<GitBranch^>
+		{
+			initonly GitRepository ^_repository;
+		internal:
+			GitBranchCollection(GitRepository ^repository)
+			{
+				if (!repository)
+					throw gcnew ArgumentNullException("repository");
+
+				_repository = repository;
+			}
+
+		public:
+			virtual System::Collections::Generic::IEnumerator<GitBranch^>^ GetEnumerator();
+
+		public:
+			property IEnumerable<GitBranch^>^ Local
+			{
+				IEnumerable<GitBranch^>^ get();
+			}
+
+			property IEnumerable<GitBranch^>^ Remote
+			{
+				IEnumerable<GitBranch^>^ get();
+			}
+
+		public:
+			bool Create(GitCommit^ commit, String^ name);
+			bool Create(GitCommit^ commit, String^ name, GitArgs^ args);
+			bool Create(GitCommit^ commit, String^ name, [Out] GitBranch^% branch);
+			bool Create(GitCommit^ commit, String^ name, GitArgs^ args, [Out] GitBranch^% branch);
+
+		private:
+			IEnumerable<GitBranch^>^ GetEnumerable(git_branch_t flags);
+
+		public:
+			//bool Contains(GitReference ^reference);
+
+		private:
+			virtual System::Collections::IEnumerator^ GetObjectEnumerator() sealed = System::Collections::IEnumerable::GetEnumerator
+			{
+				return GetEnumerator();
+			}
+		};
+	}
+}
--- trunk/src/SharpGit/Plumbing/GitTag.cpp
+++ trunk/src/SharpGit/Plumbing/GitTag.cpp
@@ -4,14 +4,18 @@
 #include "GitObject.h"
 #include "GitBlob.h"
 #include "GitCommit.h"
+#include "GitReference.h"
 #include "GitTag.h"
 #include "GitTree.h"
+#include "GitBranch.h"
 
 #include "../GitClient/GitCommitCmd.h"
 #include "../GitClient/GitTagCmd.h"
 
 using namespace SharpGit;
+using namespace SharpGit::Implementation;
 using namespace SharpGit::Plumbing;
+using System::Collections::Generic::List;
 
 GitSignature^ GitTag::Tagger::get()
 {
@@ -94,18 +98,18 @@ bool GitObject::Tag(String ^tagName, GitTagArgs ^args, [Out] GitId ^%id)
 			msg = args->LogMessage ? pool.AllocString(args->LogMessage) : "";
 
 		r = git_tag_create(&result, Repository->Handle,
-						   pool.AllocString(tagName),
-						   Handle,
-						   args->Tagger->Alloc(Repository, %pool),
-						   msg,
-						   args->OverwriteExisting);
+			pool.AllocString(tagName),
+			Handle,
+			args->Tagger->Alloc(Repository, %pool),
+			msg,
+			args->OverwriteExisting);
 	}
 	else
 	{
 		r = git_tag_create_lightweight(&result, Repository->Handle,
-									   pool.AllocString(tagName),
-									   Handle,
-									   args->OverwriteExisting);
+			pool.AllocString(tagName),
+			Handle,
+			args->OverwriteExisting);
 	}
 
 	if (r == 0)
@@ -115,3 +119,117 @@ bool GitObject::Tag(String ^tagName, GitTagArgs ^args, [Out] GitId ^%id)
 
 	return args->HandleGitError(this, r);
 }
+
+GitBranchCollection^ GitRepository::Branches::get()
+{
+	if (! _branches)
+		_branches = gcnew GitBranchCollection(this);
+
+	return _branches;
+}
+
+GitReference^ GitBranch::Reference::get() 
+{
+	if (!_reference && !_repository->IsDisposed)
+		_reference = gcnew GitReference(_repository, Name);
+
+	return _reference;
+}
+
+IEnumerator<GitBranch^>^ GitBranchCollection::GetEnumerator()
+{
+	IEnumerable<GitBranch^>^ walker = GetEnumerable((git_branch_t)(GIT_BRANCH_LOCAL | GIT_BRANCH_REMOTE));
+
+	return walker->GetEnumerator();
+}
+
+IEnumerable<GitBranch^>^ GitBranchCollection::Local::get()
+{
+	return GetEnumerable(GIT_BRANCH_LOCAL);
+}
+
+IEnumerable<GitBranch^>^ GitBranchCollection::Remote::get()
+{
+	return GetEnumerable(GIT_BRANCH_REMOTE);
+}
+
+private ref class BranchWalkInfo
+{
+public:
+	List<GitBranch^>^ branches;
+	GitRepository^ repository;
+};
+
+static int __cdecl for_branch(const char *branch_name, git_branch_t branch_type, void *payload)
+{
+	GitRoot<BranchWalkInfo^> root(payload);
+
+	root->branches->Add(gcnew GitBranch(root->repository, GitBase::Utf8_PtrToString(branch_name), branch_type));
+
+	return 0;
+}
+
+IEnumerable<GitBranch^>^ GitBranchCollection::GetEnumerable(git_branch_t flags)
+{
+	List<GitBranch^> ^branches = gcnew List<GitBranch^>();
+	if (_repository->IsDisposed)
+		return branches->AsReadOnly();
+
+	GitRoot<BranchWalkInfo^> root(gcnew BranchWalkInfo());
+	root->branches = branches;
+	root->repository = _repository;
+
+	int r = git_branch_foreach(_repository->Handle, flags, for_branch, root.GetBatonValue());
+	if (r != 0)
+		branches->Clear();
+
+	return branches->AsReadOnly();
+}
+
+bool GitBranchCollection::Create(GitCommit^ commit, String^ name)
+{
+	GitBranch ^ignored;
+	return Create(commit, name, gcnew GitNoArgs(), ignored);
+}
+
+bool GitBranchCollection::Create(GitCommit^ commit, String^ name, GitArgs^ args)
+{
+	GitBranch ^ignored;
+	return Create(commit, name, args, ignored);
+}
+
+bool GitBranchCollection::Create(GitCommit^ commit, String^ name, [Out] GitBranch^% branch)
+{
+	return Create(commit, name, gcnew GitNoArgs(), branch);
+}
+
+bool GitBranchCollection::Create(GitCommit^ commit, String^ name, GitArgs^ args, [Out] GitBranch^% branch)
+{
+	if (_repository->IsDisposed)
+		throw gcnew ObjectDisposedException("repository");
+	else if (!commit)
+		throw gcnew ArgumentNullException("commit");
+	else if (String::IsNullOrEmpty(name))
+		throw gcnew ArgumentNullException("name");
+	else if(commit->IsDisposed || commit->Repository != _repository)
+		throw gcnew ArgumentOutOfRangeException("commit");
+
+	GitPool pool(_repository->Pool);
+	git_reference *result;
+	int r = git_branch_create(&result, _repository->Handle, pool.AllocString(name), commit->Handle, FALSE /* force */);
+
+	if (!r)
+		branch = gcnew GitBranch(_repository, name, gcnew GitReference(_repository, result));
+	else
+		branch = nullptr;
+
+	return args->HandleGitError(this, r);
+}
+
+/*bool GitBranchCollection::Contains(GitReference ^reference)
+{
+if (_repository->IsDisposed)
+return false;
+
+int r = git_branch_lookup(
+}*/
