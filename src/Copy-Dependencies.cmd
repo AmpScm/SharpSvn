a76e6c72d033faff970765b25fc55d13c0411383
Bert Huijben <rhuijben@users.noreply.github.com>
2013-08-25 21:22:21 +0000

    Exit error on copy failures

--- src/Copy-Dependencies.cmd
+++ src/Copy-Dependencies.cmd
@@ -1,9 +1,11 @@
 @echo off
+SETLOCAL ENABLEEXTENSIONS
 set TO=%1
 set CONFIGURATION=%2
 set PLATFORM=%3
 set SUFFIX=%4
 xcopy /r /y /q /i "..\..\imports\release\bin\*-%PLATFORM%*dll" %TO% || echo Copying dependencies failed
+IF ERRORLEVEL 1 EXIT /B 1
 copy "..\SharpPlink\bin%SUFFIX%\%PLATFORM%\%CONFIGURATION%\SharpPlink-%PLATFORM%.exe" %TO%\SharpPlink-%PLATFORM%.svnExe || echo Copying SharpPlink failed
-
-exit 0
+IF ERRORLEVEL 1 EXIT /B 1
+EXIT /B 0
