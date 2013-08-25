17188466b91f953a97df8f7bcfa4efb40df5dd30
Bert Huijben <rhuijben@users.noreply.github.com>
2013-08-25 22:08:31 +0000

    Make directory if necessary

--- src/Copy-Dependencies.cmd
+++ src/Copy-Dependencies.cmd
@@ -4,6 +4,7 @@ set TO=%1
 set CONFIGURATION=%2
 set PLATFORM=%3
 set SUFFIX=%4
+IF NOT EXIST "%TO%" mkdir "%TO%"
 copy "..\SharpPlink\bin%SUFFIX%\%PLATFORM%\%CONFIGURATION%\SharpPlink-%PLATFORM%.exe" %TO%\SharpPlink-%PLATFORM%.svnExe || echo Copying SharpPlink failed
 IF ERRORLEVEL 1 EXIT /B 1
 EXIT /B 0
