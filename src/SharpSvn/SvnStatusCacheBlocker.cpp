1744fa9d22815bb4ee2ad0d24b645f41cb9e1e7f
Bert Huijben <rhuijben@users.noreply.github.com>
2013-12-04 13:02:56 +0000

    Switch to a dual solution/project layout: Just 2008 and 2010 and later.

--- src/SharpSvn/SvnStatusCacheBlocker.cpp
+++ src/SharpSvn/SvnStatusCacheBlocker.cpp
@@ -81,6 +81,26 @@ bool SvnStatusCacheBlocker::SendCommand(Byte cmd, String^ value)
     else if (value->Length > MAX_PATH)
         return false; /* Not possible with this protocol */
 
+    /* TSVN 1.8:
+
+    CString t;
+    CAutoGeneralHandle token;
+    BOOL result = OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, token.GetPointer());
+    if(result)
+    {
+        DWORD len = 0;
+        GetTokenInformation(token, TokenStatistics, NULL, 0, &len);
+        if (len >= sizeof (TOKEN_STATISTICS))
+        {
+            std::unique_ptr<BYTE[]> data (new BYTE[len]);
+            GetTokenInformation(token, TokenStatistics, data.get(), len, &len);
+            LUID uid = ((PTOKEN_STATISTICS)data.get())->AuthenticationId;
+            t.Format(_T("-%08x%08x"), uid.HighPart, uid.LowPart);
+        }
+    }
+    return t;
+    */
+
     String ^sChannel = String::Format("\\\\.\\pipe\\TSVNCacheCommand-{0:X16}", WindowsLogonSessionId);
     pin_ptr<const wchar_t> pChannel = PtrToStringChars(sChannel);
 
