258b428cea678b4869486247059236bfe59c0430
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2013-12-05 17:20:42 +0000

    * src/SharpSvn/Commands/WriteRelated.cpp   Following up on r2741, fix compilation.

--- trunk/src/SharpSvn/Commands/WriteRelated.cpp
+++ trunk/src/SharpSvn/Commands/WriteRelated.cpp
@@ -32,12 +32,12 @@ using System::Collections::Generic::List;
 
 bool SvnClient::WriteRelated(ICollection<SvnUriTarget^>^ targets, ICollection<Stream^>^ to)
 {
-	if (!targets)
-		throw gcnew ArgumentNullException("targets");
-	else if (!to)
-		throw gcnew ArgumentNullException("to");
+    if (!targets)
+        throw gcnew ArgumentNullException("targets");
+    else if (!to)
+        throw gcnew ArgumentNullException("to");
 
-	return WriteRelated(targets, to, gcnew SvnWriteRelatedArgs());
+    return WriteRelated(targets, to, gcnew SvnWriteRelatedArgs());
 }
 
 /*****
@@ -55,179 +55,180 @@ bool SvnClient::WriteRelated(ICollection<SvnUriTarget^>^ targets, ICollection<St
 ref class WriteRelatedEditor : Delta::SvnDeltaEditor
 {
 protected public:
-	virtual void OnSetTarget(SvnDeltaSetTargetEventArgs^ e) override
-	{
-		__super::OnSetTarget(e);
-	}
+    virtual void OnSetTarget(SvnDeltaSetTargetEventArgs^ e) override
+    {
+        __super::OnSetTarget(e);
+    }
 
-	virtual void OnFilePropertyChange(SvnDeltaFilePropertyChangeEventArgs^ e) override
-	{
-		__super::OnFilePropertyChange(e);
-	}
+    virtual void OnFilePropertyChange(SvnDeltaFilePropertyChangeEventArgs^ e) override
+    {
+        __super::OnFilePropertyChange(e);
+    }
 
-	virtual void OnFileChange(SvnDeltaFileChangeEventArgs^ e) override
-	{
-		__super::OnFileChange(e);
-	}
+    virtual void OnFileChange(SvnDeltaFileChangeEventArgs^ e) override
+    {
+        __super::OnFileChange(e);
+    }
 };
 
 bool SvnClient::WriteRelated(ICollection<SvnUriTarget^>^ targets, ICollection<Stream^>^ to, SvnWriteRelatedArgs^ args)
 {
-	if (!targets)
-		throw gcnew ArgumentNullException("targets");
-	else if (!to)
-		throw gcnew ArgumentNullException("to");
-	else if (!args)
-		throw gcnew ArgumentNullException("args");
-	else if (!targets->Count)
-		throw gcnew ArgumentException(SharpSvnStrings::CollectionMustContainAtLeastOneItem, "targets");
-	else if (targets->Count != to->Count)
-		throw gcnew ArgumentException("Collection lengths must match", "targets");
+    if (!targets)
+        throw gcnew ArgumentNullException("targets");
+    else if (!to)
+        throw gcnew ArgumentNullException("to");
+    else if (!args)
+        throw gcnew ArgumentNullException("args");
+    else if (!targets->Count)
+        throw gcnew ArgumentException(SharpSvnStrings::CollectionMustContainAtLeastOneItem, "targets");
+    else if (targets->Count != to->Count)
+        throw gcnew ArgumentException("Collection lengths must match", "targets");
 
-	for each (SvnUriTarget^ t in targets)
-	{
-		if (!t)
-			throw gcnew ArgumentException(SharpSvnStrings::ItemInListIsNull, "targets");
-	}
+    for each (SvnUriTarget^ t in targets)
+    {
+        if (!t)
+            throw gcnew ArgumentException(SharpSvnStrings::ItemInListIsNull, "targets");
+    }
 
-	for each (Stream^ t in to)
-	{
-		if (!t)
-			throw gcnew ArgumentException(SharpSvnStrings::ItemInListIsNull, "to");
-	}
+    for each (Stream^ t in to)
+    {
+        if (!t)
+            throw gcnew ArgumentException(SharpSvnStrings::ItemInListIsNull, "to");
+    }
 
-	array<SvnUriTarget^>^ theTargets = gcnew array<SvnUriTarget^>(targets->Count);
-	array<Stream^>^ theStreams = gcnew array<Stream^>(to->Count);
+    array<SvnUriTarget^>^ theTargets = gcnew array<SvnUriTarget^>(targets->Count);
+    array<Stream^>^ theStreams = gcnew array<Stream^>(to->Count);
 
-	targets->CopyTo(theTargets, 0);
-	to->CopyTo(theStreams, 0);
+    targets->CopyTo(theTargets, 0);
+    to->CopyTo(theStreams, 0);
 
-	EnsureState(SvnContextState::AuthorizationInitialized);
-	AprPool pool(%_pool);
-	ArgsStore store(this, args, %pool);
-	AprPool^ cur = gcnew AprPool(%pool);
-	AprPool^ next = gcnew AprPool(%pool);
-	WriteRelatedEditor^ wrEditor = gcnew WriteRelatedEditor();
-	//if (versionHandler)
-	//	args->FileVersion += versionHandler;
-	try
-	{
-		svn_ra_session_t* ra_session = nullptr;
+    EnsureState(SvnContextState::AuthorizationInitialized);
+    AprPool pool(%_pool);
+    ArgsStore store(this, args, %pool);
+    AprPool^ cur = gcnew AprPool(%pool);
+    AprPool^ next = gcnew AprPool(%pool);
+    WriteRelatedEditor^ wrEditor = gcnew WriteRelatedEditor();
+    //if (versionHandler)
+    //	args->FileVersion += versionHandler;
+    try
+    {
+        svn_ra_session_t* ra_session = nullptr;
 
-		const char* baseUrl = nullptr;
-		svn_revnum_t baseRev;
+        const char* baseUrl = nullptr;
+        svn_revnum_t baseRev;
 
-		svn_client__pathrev_t *pathrev;
-		svn_error_t* r;
-		svn_revnum_t rev_head;
+        svn_client__pathrev_t *pathrev;
+        svn_error_t* r;
+        svn_revnum_t rev_head;
 
-		// if (args->WcBase)
-		// {
-		// }
-		// else
-		{
-			SvnUriTarget^ first = FirstOf(targets);
-			const char* pTarget = first->AllocAsString(%pool);
+        // if (args->WcBase)
+        // {
+        // }
+        // else
+        {
+            SvnUriTarget^ first = FirstOf(targets);
+            const char* pTarget = first->AllocAsString(%pool);
 
-			r = svn_client__ra_session_from_path2(
-				&ra_session,
-				&pathrev,
-				pTarget,
-				nullptr,
-				first->Revision->AllocSvnRevision(%pool),
-				first->Revision->AllocSvnRevision(%pool),
-				CtxHandle,
-				pool.Handle);
+            r = svn_client__ra_session_from_path2(
+                &ra_session,
+                &pathrev,
+                pTarget,
+                nullptr,
+                first->Revision->AllocSvnRevision(%pool),
+                first->Revision->AllocSvnRevision(%pool),
+                CtxHandle,
+                pool.Handle);
 
-			if (r)
-				return args->HandleResult(this, r, targets);
+            if (r)
+                return args->HandleResult(this, r, targets);
 
-			// Get the single revision to use as head for the next calls
-			r = svn_ra_get_latest_revnum(ra_session, &rev_head, pool.Handle);
+            // Get the single revision to use as head for the next calls
+            r = svn_ra_get_latest_revnum(ra_session, &rev_head, pool.Handle);
 
-			if (r)
-				return args->HandleResult(this, r, targets);
+            if (r)
+                return args->HandleResult(this, r, targets);
 
-			baseUrl = pathrev->url;
-			baseRev = 0; // Nothing exists
-		}
+            baseUrl = pathrev->url;
+            baseRev = 0; // Nothing exists
+        }
 
-		const svn_delta_editor_t* editor;
-		void* editor_baton;
-		wrEditor->AllocEditor(this, &editor, &editor_baton, %pool);
+        const svn_delta_editor_t* editor;
+        void* editor_baton;
+        wrEditor->AllocEditor(this, &editor, &editor_baton, %pool);
 
-		for each(SvnUriTarget^ ta in targets)
-		{
-			cur->Clear();
+        for each(SvnUriTarget^ ta in targets)
+        {
+            cur->Clear();
 
-			svn_revnum_t switchToRevision = (svn_revnum_t)ta->Revision->Revision;
-			const char* switchUrl = ta->AllocAsString(cur);
+            svn_revnum_t switchToRevision = (svn_revnum_t)ta->Revision->Revision;
+            const char* switchUrl = ta->AllocAsString(cur);
 
-			const char* baseUrlDir;
-			const char* baseUrlFile;
-			svn_uri_split(&baseUrlDir, &baseUrlFile, baseUrl, cur->Handle);
+            const char* baseUrlDir;
+            const char* baseUrlFile;
+            svn_uri_split(&baseUrlDir, &baseUrlFile, baseUrl, cur->Handle);
 
-			const char* switchUrlDir;
-			const char* switchUrlFile;
-			svn_uri_split(&switchUrlDir, &switchUrlFile, switchUrl, cur->Handle);
+            const char* switchUrlDir;
+            const char* switchUrlFile;
+            svn_uri_split(&switchUrlDir, &switchUrlFile, switchUrl, cur->Handle);
 
-			r = svn_ra_reparent(ra_session, baseUrlDir, cur->Handle);
+            r = svn_ra_reparent(ra_session, baseUrlDir, cur->Handle);
 
-			if (r)
-				return args->HandleResult(this, r, targets);
+            if (r)
+                return args->HandleResult(this, r, targets);
 
-			const svn_ra_reporter3_t* reporter = nullptr;
-			void* report_baton = nullptr;
+            const svn_ra_reporter3_t* reporter = nullptr;
+            void* report_baton = nullptr;
 
-			r = svn_ra_do_switch3(ra_session, &reporter, &report_baton,
-				switchToRevision,
-				baseUrlFile,
-				svn_depth_empty,
-				switchUrl,
-				FALSE /* send_copyfrom_args */,
-				TRUE /* ignore_ancestry */,
-				editor,
-				editor_baton,
-				cur->Handle);
+            r = svn_ra_do_switch3(ra_session, &reporter, &report_baton,
+                switchToRevision,
+                baseUrlFile,
+                svn_depth_empty,
+                switchUrl,
+                FALSE /* send_copyfrom_args */,
+                TRUE /* ignore_ancestry */,
+                editor,
+                editor_baton,
+                cur->Handle,
+                cur->Handle);
 
-			if (r)
-				return args->HandleResult(this, r, targets);
+            if (r)
+                return args->HandleResult(this, r, targets);
 
-			r = reporter->set_path(
-					report_baton,
-					"",
-					baseRev,
-					svn_depth_empty,
-					false,
-					nullptr,
-					cur->Handle);
+            r = reporter->set_path(
+                report_baton,
+                "",
+                baseRev,
+                svn_depth_empty,
+                false,
+                nullptr,
+                cur->Handle);
 
-			if (r)
-				return args->HandleResult(this, r, targets);
+            if (r)
+                return args->HandleResult(this, r, targets);
 
-			// Close the reporter, this will start the editor walk
-			r = reporter->finish_report(report_baton, cur->Handle);
+            // Close the reporter, this will start the editor walk
+            r = reporter->finish_report(report_baton, cur->Handle);
 
-			if (r)
-				return args->HandleResult(this, r, targets);
+            if (r)
+                return args->HandleResult(this, r, targets);
 
-			baseUrl = switchUrl;
-			baseRev = switchToRevision;
+            baseUrl = switchUrl;
+            baseRev = switchToRevision;
 
-			{
-				// Swap pools for next action
-				AprPool^ tmp = next;
-				next = cur;
-				cur = tmp;
-			}
-		}
+            {
+                // Swap pools for next action
+                AprPool^ tmp = next;
+                next = cur;
+                cur = tmp;
+            }
+        }
 
 
-		return true;
-	}
-	finally
-	{
-	}
+        return true;
+    }
+    finally
+    {
+    }
 }
 
 #endif
