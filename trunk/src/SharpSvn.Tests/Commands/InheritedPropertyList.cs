842529d8c91dac08b4c765129b7e90bce4032b56
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2013-12-11 13:42:53 +0000

    * src/SharpSvn.Tests/Commands/InheritedPropertyList.cs   Remove unused usings to fix compilation with 2.0

--- trunk/src/SharpSvn.Tests/Commands/InheritedPropertyList.cs
+++ trunk/src/SharpSvn.Tests/Commands/InheritedPropertyList.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 
 namespace SharpSvn.Tests.Commands
