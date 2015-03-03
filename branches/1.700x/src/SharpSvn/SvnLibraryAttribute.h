e09f430a9878a67619f022fc68cd5e2527f60cb8
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2015-03-03 12:09:30 +0000

    Merge r3523 from trunk: Add version number to SharpPlink

--- branches/1.700x/src/SharpSvn/SvnLibraryAttribute.h
+++ branches/1.700x/src/SharpSvn/SvnLibraryAttribute.h
@@ -1,4 +1,4 @@
-// $Id: AprArray.h 534 2008-04-14 22:52:59Z rhuijben $
+
 //
 // Copyright 2007-2008 The SharpSvn Project
 //
@@ -17,171 +17,155 @@
 #pragma once
 
 namespace SharpSvn {
-	namespace Implementation {
+    namespace Implementation {
 
-		[System::AttributeUsage(AttributeTargets::Assembly, AllowMultiple=true)]
-		public ref class SvnLibraryAttribute sealed : public System::Attribute
-		{
-			initonly String^ _name;
-			initonly String^ _version;
-			bool _dynamicallyLinked;
-			bool _skipPrefix;
-			bool _optional;
-			bool _useSharpSvnVersion;
+        [System::AttributeUsage(AttributeTargets::Assembly, AllowMultiple=true)]
+        public ref class SvnLibraryAttribute sealed : public System::Attribute
+        {
+            initonly String^ _name;
+            initonly String^ _version;
+            bool _dynamicallyLinked;
+            bool _skipPrefix;
+            bool _optional;
 
-		public:
-			SvnLibraryAttribute(String^ name, String^ version)
-			{
-				if (String::IsNullOrEmpty(name))
-					throw gcnew ArgumentNullException("name");
-				else if (!version)
-					throw gcnew ArgumentNullException("version");
+        public:
+            SvnLibraryAttribute(String^ name, String^ version)
+            {
+                if (String::IsNullOrEmpty(name))
+                    throw gcnew ArgumentNullException("name");
+                else if (!version)
+                    throw gcnew ArgumentNullException("version");
 
-				_name = name;
-				_version = version;
-			}
+                _name = name;
+                _version = version;
+            }
 
-		public:
-			property String^ Name
-			{
-				String^ get()
-				{
-					return _name;
-				}
-			}
+        public:
+            property String^ Name
+            {
+                String^ get()
+                {
+                    return _name;
+                }
+            }
 
-			property String^ Version
-			{
-				String^ get()
-				{
-					return _version;
-				}
-			}
+            property String^ Version
+            {
+                String^ get()
+                {
+                    return _version;
+                }
+            }
 
-			property bool DynamicallyLinked
-			{
-				bool get()
-				{
-					return _dynamicallyLinked;
-				}
-				void set(bool value)
-				{
-					_dynamicallyLinked = value;
-				}
-			}
+            property bool DynamicallyLinked
+            {
+                bool get()
+                {
+                    return _dynamicallyLinked;
+                }
+                void set(bool value)
+                {
+                    _dynamicallyLinked = value;
+                }
+            }
 
-			property bool SkipPrefix
-			{
-				bool get()
-				{
-					return _skipPrefix;
-				}
-				void set(bool value)
-				{
-					_skipPrefix = value;
-				}
-			}
+            property bool SkipPrefix
+            {
+                bool get()
+                {
+                    return _skipPrefix;
+                }
+                void set(bool value)
+                {
+                    _skipPrefix = value;
+                }
+            }
 
-			property bool Optional
-			{
-				bool get()
-				{
-					return _optional;
-				}
-				void set(bool value)
-				{
-					_optional = value;
-				}
-			}
+            property bool Optional
+            {
+                bool get()
+                {
+                    return _optional;
+                }
+                void set(bool value)
+                {
+                    _optional = value;
+                }
+            }
+        };
 
-			property bool UseSharpSvnVersion
-			{
-				bool get()
-				{
-					return _useSharpSvnVersion;
-				}
-				void set(bool value)
-				{
-					_useSharpSvnVersion = value;
-				}
-			}
-		};
+        public ref class SvnLibrary sealed
+        {
+            initonly String^ _name;
+            initonly String^ _version;
+            initonly bool _dynamicallyLinked;
+            initonly System::Version^ _versionData;
+            initonly bool _optional;
 
-		public ref class SvnLibrary sealed
-		{
-			initonly String^ _name;
-			initonly String^ _version;
-			initonly bool _dynamicallyLinked;
-			initonly System::Version^ _versionData;
-			initonly bool _optional;
+        internal:
+            SvnLibrary(SvnLibraryAttribute^ attribute)
+            {
+                if (!attribute)
+                    throw gcnew ArgumentNullException("attribute");
 
-		internal:
-			SvnLibrary(SvnLibraryAttribute^ attribute)
-			{
-				if (!attribute)
-					throw gcnew ArgumentNullException("attribute");
+                _name = attribute->Name;
+                _version = attribute->Version;
 
-				_name = attribute->Name;
-				_version = attribute->Version;
+                System::Text::RegularExpressions::Match^ m =
+                    System::Text::RegularExpressions::Regex::Match(_version, "\\d+\\.(\\d+\\.)*\\d+");
 
-				if(attribute->UseSharpSvnVersion)
-					_version = (gcnew System::Reflection::AssemblyName(SvnLibrary::typeid->Assembly->FullName))->Version->ToString();
+                if (m->Success)
+                {
+                    _versionData = gcnew System::Version(m->Value);
 
-				_dynamicallyLinked = attribute->DynamicallyLinked;
+                    if (attribute->SkipPrefix)
+                        _version = _version->Substring(m->Index);
+                }
 
-				System::Text::RegularExpressions::Match^ m =
-					System::Text::RegularExpressions::Regex::Match(_version, "\\d+\\.(\\d+\\.)*\\d+");
+                _dynamicallyLinked = attribute->DynamicallyLinked;
+                _optional = attribute->Optional;
+            }
 
-				if (m->Success)
-				{
-					_versionData = gcnew System::Version(m->Value);
+        public:
+            property String^ Name
+            {
+                String^ get()
+                {
+                    return _name;
+                }
+            }
 
-					if (attribute->SkipPrefix)
-						_version = _version->Substring(m->Index);
-				}
-				_optional = attribute->Optional;
-			}
+            property String^ VersionString
+            {
+                String^ get()
+                {
+                    return _version;
+                }
+            }
 
-		public:
-			property String^ Name
-			{
-				String^ get()
-				{
-					return _name;
-				}
-			}
+            property System::Version^ Version
+            {
+                System::Version^ get()
+                {
+                    return _versionData;
+                }
+            }
 
-			property String^ VersionString
-			{
-				String^ get()
-				{
-					return _version;
-				}
-			}
+            property bool DynamicallyLinked
+            {
+                bool get()
+                {
+                    return _dynamicallyLinked;
+                }
+            }
 
-			property System::Version^ Version
-			{
-				System::Version^ get()
-				{
-					return _versionData;
-				}
-			}
-
-			property bool DynamicallyLinked
-			{
-				bool get()
-				{
-					return _dynamicallyLinked;
-				}
-			}
-
-			property bool Optional
-			{
-				bool get()
-				{
-					return _optional;
-				}
-			}
-		};
-	}
+            property bool Optional
+            {
+                bool get()
+                {
+                    return _optional;
+                }
+            }
+        };
+    }
 }
