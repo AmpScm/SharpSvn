6890288949071d320c61d14bb70ebeff639c9828
Bert Huijben <rhuijben@users.noreply.github.com>
2015-03-20 13:20:54 +0000

    Merge r3579 from trunk: Allow passing old revision property value

--- src/SharpSvn/Args/SetRevisionProperty.h
+++ src/SharpSvn/Args/SetRevisionProperty.h
@@ -1,5 +1,3 @@
-// $Id$
-//
 // Copyright 2007-2008 The SharpSvn Project
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,35 +16,67 @@
 
 namespace SharpSvn {
 
-	/// <summary>Extended Parameter container of <see cref="SvnClient::SetRevisionProperty(SvnUriTarget^,String^,SvnSetRevisionPropertyArgs^,String^)" /></summary>
-	/// <threadsafety static="true" instance="false"/>
-	public ref class SvnSetRevisionPropertyArgs : public SvnClientArgs
-	{
-		bool _force;
-	public:
-		SvnSetRevisionPropertyArgs()
-		{
-		}
+    using namespace System;
 
-		virtual property SvnCommandType CommandType
-		{
-			virtual SvnCommandType get() override sealed
-			{
-				return SvnCommandType::SetRevisionProperty;
-			}
-		}
+    /// <summary>Extended Parameter container of <see cref="SvnClient::SetRevisionProperty(SvnUriTarget^,String^,SvnSetRevisionPropertyArgs^,String^)" /></summary>
+    /// <threadsafety static="true" instance="false"/>
+    public ref class SvnSetRevisionPropertyArgs : public SvnClientArgs
+    {
+        bool _force;
+        String ^_oldValue;
+        array<Byte> ^_oldValueBytes;
+    public:
+        SvnSetRevisionPropertyArgs()
+        {
+        }
 
-		property bool Force
-		{
-			bool get()
-			{
-				return _force;
-			}
-			void set(bool value)
-			{
-				_force = value;
-			}
-		}
-	};
+        virtual property SvnCommandType CommandType
+        {
+            virtual SvnCommandType get() override sealed
+            {
+                return SvnCommandType::SetRevisionProperty;
+            }
+        }
+
+        property bool Force
+        {
+            bool get()
+            {
+                return _force;
+            }
+            void set(bool value)
+            {
+                _force = value;
+            }
+        }
+
+        property String ^OldValue
+        {
+            String ^get()
+            {
+                return _oldValue;
+            }
+            void set(String ^value)
+            {
+                _oldValue = value;
+                if (value)
+                    _oldValueBytes = nullptr;
+            }
+        }
+
+        property array<Byte> ^OldRawValue
+        {
+            array<Byte> ^get()
+            {
+                return _oldValueBytes;
+            }
+            void set(array<Byte> ^ value)
+            {
+                _oldValueBytes = (array<Byte> ^)(value ? value->Clone() : nullptr);
+                if (value)
+                    _oldValue = nullptr;
+            }
+        }
+    };
 
 }
