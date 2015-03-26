acbbfaf7a273318b37e0a82004a9848576335d4f
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2015-03-26 12:23:07 +0000

    * AnonymizedFileDumper.sln * AnonymizedFileDumper/AnonymizedFileDumper.csproj * AnonymizedFileDumper/Anonymyzer.cs * AnonymizedFileDumper/App.config * AnonymizedFileDumper/packages.config * AnonymizedFileDumper/Program.cs * AnonymizedFileDumper/Properties/AssemblyInfo.cs   Add project.

---
+++ tests/FileDumper/trunk/AnonymizedFileDumper/Anonymyzer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Anonymyzer : Stream
+{
+    readonly Stream _inner;
+    readonly HashAlgorithm _hash;
+    readonly StreamWriter _writer;
+
+    public Anonymyzer(Stream toStream)
+    {
+        _inner = toStream;
+        _hash = new MD5Cng();
+        _writer = new StreamWriter(_inner);
+    }
+
+    public override bool CanRead
+    {
+        get { return false; }
+    }
+
+    public override bool CanSeek
+    {
+        get { return false; }
+    }
+
+    public override bool CanWrite
+    {
+        get { return false; }
+    }
+
+    public override void Flush()
+    {
+    }
+
+    public override void Close()
+    {
+        PushLine();
+        base.Close();
+    }
+
+    public override long Length
+    {
+        get { return -1; }
+    }
+
+    public override long Position
+    {
+        get
+        {
+            return -1;
+        }
+        set
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    readonly List<byte> _lineBuffer = new List<byte>();
+    enum LinePosition
+    {
+        Start,
+        Body,
+    }
+
+    LinePosition _pos;
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        while (count > 0)
+        {
+            if (_pos == LinePosition.Start && IsWhite(buffer, offset))
+            {
+                _inner.WriteByte(buffer[offset]);
+                count--;
+                offset++;
+                continue;
+            }
+            _pos = LinePosition.Body;
+            if (buffer[offset] == '\n')
+            {
+                PushLine();
+                _inner.WriteByte((byte)'\n');
+                _pos = LinePosition.Start;
+            }
+            else
+            {
+                _lineBuffer.Add(buffer[offset]);
+            }
+            offset++;
+            count--;
+        }
+    }
+
+    private void PushLine()
+    {
+        int nSpace = 0; // Nr of whitespace characters at end
+        while (nSpace < _lineBuffer.Count && IsWhite(_lineBuffer, _lineBuffer.Count - nSpace - 1))
+            nSpace++;
+
+        _writer.Write(CalcHash(_lineBuffer, 0, _lineBuffer.Count - nSpace));
+        _writer.Flush();
+        if (nSpace > 0)
+            _inner.Write(_lineBuffer.ToArray(), _lineBuffer.Count - nSpace, nSpace);
+        _lineBuffer.Clear();
+    }
+
+    private string CalcHash(List<byte> buffer, int offset, int count)
+    {
+        if (count > 0)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in _hash.ComputeHash(buffer.ToArray(), offset, count))
+            {
+                sb.AppendFormat("{0:X2}", b);
+            }
+            return sb.ToString();
+        }
+        else
+            return "";
+    }
+
+    private bool IsWhite(IList<byte> buffer, int offset)
+    {
+        char c = (char)buffer[offset];
+
+        return c != '\n' && Char.IsWhiteSpace(c);
+    }
+}
---
+++ tests/FileDumper/trunk/AnonymizedFileDumper/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using SharpSvn;
+using SharpSvn.UI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Uri uri = null;
+        if (args.Length < 2
+            || !Uri.TryCreate(args[0], UriKind.Absolute, out uri))
+        {
+            Console.Error.WriteLine("Usage: SvnDumpFileToRepository URL PATH");
+        }
+
+        string reposPath = args[1];
+        if (Directory.Exists(reposPath))
+        {
+            foreach (FileInfo f in new DirectoryInfo(reposPath).GetFiles("*", SearchOption.AllDirectories))
+                f.Attributes = f.Attributes & ~FileAttributes.ReadOnly;
+
+            Directory.Delete(reposPath, true);
+        }
+
+        using (SvnRepositoryClient repos = new SvnRepositoryClient())
+        {
+            repos.CreateRepository(reposPath);
+        }
+
+        Uri reposUri = SvnTools.LocalPathToUri(args[1], true);
+
+        using (SvnClient client = new SvnClient())
+        using (SvnClient client2 = new SvnClient())
+        {
+            SvnUI.Bind(client, new SvnUIBindArgs());
+
+            string fileName = SvnTools.GetFileName(uri);
+            bool create = true;
+            client.FileVersions(uri,
+                delegate(object sender, SvnFileVersionEventArgs e)
+                {
+                    Console.WriteLine("Copying {0} in r{1}", fileName, e.Revision);
+
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        e.WriteTo(new Anonymyzer(ms)); // Write full text to memory stream
+                        ms.Position = 0;
+
+                        // And now use 'svnmucc' to update repository
+                        client2.RepositoryOperation(reposUri,
+                            new SvnRepositoryOperationArgs { LogMessage = string.Format("r{0}", e.Revision) },
+                            delegate(SvnMultiCommandClient mucc)
+                            {
+                                if (create)
+                                {
+                                    mucc.CreateFile(fileName, ms);
+                                    create = false;
+                                }
+                                else
+                                    mucc.UpdateFile(fileName, ms);
+                            });
+                    }
+                });
+        }
+    }
+}
