cd84d0e7a2b99ec3a6f246282d13fc35f0686c92
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2017-07-27 10:03:44 +0000

    * src/SharpPlink/sharpplink-ex.h   Following up on r3966, assume included before Windows.h

--- trunk/src/SharpPlink/sharpplink-ex.h
+++ trunk/src/SharpPlink/sharpplink-ex.h
@@ -1,5 +1,6 @@
 #pragma once
 
 #if defined _MSC_VER && _MSC_VER < 1900
-typedef PVOID DLL_DIRECTORY_COOKIE, *PDLL_DIRECTORY_COOKIE;
+typedef void *   DLL_DIRECTORY_COOKIE;
+typedef void ** PDLL_DIRECTORY_COOKIE;
 #endif
