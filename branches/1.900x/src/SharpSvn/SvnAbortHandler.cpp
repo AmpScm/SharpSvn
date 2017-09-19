11e8897a38dd72806ecebaa74bececcc5a0f6bd7
rhuijben <rhuijben@0f10076b-13ac-4405-9e11-06ee11480df6>
2017-09-19 09:53:07 +0000

    Sync with trunk

--- branches/1.900x/src/SharpSvn/SvnAbortHandler.cpp
+++ branches/1.900x/src/SharpSvn/SvnAbortHandler.cpp
@@ -13,6 +13,9 @@
 //  limitations under the License.
 
 #include "stdafx.h"
+#if _MSC_VER >= 1900
+#define DELAYIMP_INSECURE_WRITABLE_HOOKS
+#endif
 #include "DelayImp.h"
 
 #include "db.h"
@@ -20,6 +23,16 @@
 #include "svn_utf.h"
 #define SHARPSVN_NO_ABORT
 #include <libintl.h>
+
+#if _MSC_VER >= 1900
+// From header:
+// // Prior to Visual Studio 2015 Update 3, these hooks were non-const.  They were
+// // made const to improve security (global, writable function pointers are bad).
+// // If for backwards compatibility you require the hooks to be writable, define
+// // the macro DELAYIMP_INSECURE_WRITABLE_HOOKS prior to including this header and
+// // provide your own non-const definition of the hooks.
+__declspec(selectany) PfnDliHook   __pfnDliFailureHook2;
+#endif
 
 using namespace SharpSvn;
 using System::IO::Path;
