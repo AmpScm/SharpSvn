4dda511f216cd8350919e1f323b81b9c292256ba
Bert Huijben <bert@vmoo.com>
2021-05-11 16:46:50 +0200

    Update version detection. More cleanup of legacy code

--- scripts/gh-version-setup.cmd
+++ scripts/gh-version-setup.cmd
@@ -3,30 +3,36 @@ SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 
 SET CACHE=%0\..\gh.cache.bat
 SET RSPFILE=%0\..\msbuild-version.rsp
+SET VERFILE=%0\..\..\imports\release\include\svn_version.h
+
+IF NOT EXIST "%VERFILE%" (
+  echo %VERFILE% does not exist
+  exit /b 1
+)
 
 echo @echo off > %CACHE%
 
-for /F "usebackq tokens=2,3" %%i in (`"type %1 |findstr /C:_VERSION_"`) do (
+for /F "usebackq tokens=2,3" %%i in (`"type %VERFILE% |findstr /C:SVN_VER_"`) do (
   SET %%i=%%j
   echo SET %%i=%%j>> %CACHE%
 ) 
 
-set    SHARPPROJ_MAJOR=%PROJ_VERSION_MAJOR%
-set /a SHARPPROJ_MINOR=%PROJ_VERSION_MINOR% * 1000 + %PROJ_VERSION_PATCH%
-set    SHARPPROJ_PATCH=%1
+set    SHARPSVN_MAJOR=%SVN_VER_MAJOR%
+set /a SHARPSVN_MINOR=%SVN_VER_MINOR% * 1000 + %SVN_VER_PATCH%
+set    SHARPSVN_PATCH=%1
 
-echo Found PROJ %PROJ_VERSION_MAJOR%.%PROJ_VERSION_MINOR%.%PROJ_VERSION_PATCH% from header
-echo Prepare building SharpProj %PROJ_VERSION_MAJOR%.%SHARPPROJ_MINOR%.%1
+echo Found Subversion %SVN_VER_MAJOR%.%SVN_VER_MINOR%.%SVN_VER_PATCH% from header
+echo Prepare building SharpProj %SVN_VER_MAJOR%.%SHARPSVN_MINOR%.%1
 
 (
-  echo SET SHARPPROJ_MAJOR=%SHARPPROJ_MAJOR%
-  echo SET SHARPPROJ_MINOR=%SHARPPROJ_MINOR%
-  echo SET SHARPPROJ_PATCH=%SHARPPROJ_PATCH%
+  echo SET SHARPSVN_MAJOR=%SHARPSVN_MAJOR%
+  echo SET SHARPSVN_MINOR=%SHARPSVN_MINOR%
+  echo SET SHARPSVN_PATCH=%SHARPSVN_PATCH%
 ) >> %CACHE%
 
 (
-  echo /p:ForceAssemblyVersion=%SHARPPROJ_MAJOR%.%SHARPPROJ_MINOR%.%SHARPPROJ_PATCH%
-  echo /p:ForceAssemblyCompany="SharpSvb Project, powered by AmpScm, QQn & GitHub"
+  echo /p:ForceAssemblyVersion=%SHARPSVN_MAJOR%.%SHARPSVN_MINOR%.%SHARPSVN_PATCH%
+  echo /p:ForceAssemblyCompany="SharpSvn Project, powered by AmpScm, QQn & GitHub"
   echo /p:ForceAssemblyCopyright="Apache 2.0 licensed. See https://github.com/ampscm/SharpSvn"
   echo /p:BuildBotBuild=true
   echo /p:RestoreForce=true
