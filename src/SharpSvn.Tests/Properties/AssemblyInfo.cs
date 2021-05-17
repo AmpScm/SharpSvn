44cc2aaa7511b815e23348f0692963e1771393af
Bert Huijben <bert@vmoo.com>
2021-05-17 09:55:21 +0200

    Assume running tests on a modern runtime

--- src/SharpSvn.Tests/Properties/AssemblyInfo.cs
+++ src/SharpSvn.Tests/Properties/AssemblyInfo.cs
@@ -13,10 +13,10 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -50,4 +50,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
+[assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
 [assembly: AssemblyVersion("1.8999.*")]
