4b9c7b42766f9a3f62d7e27e4eb552400c26c1ff
Bert Huijben <bert@vmoo.com>
2022-01-12 17:40:33 +0100

    Use more NetCore build script and less SharpSvn annotate

--- src/SharpSvn.PdbAnnotate/Properties/AssemblyInfo.cs
+++ src/SharpSvn.PdbAnnotate/Properties/AssemblyInfo.cs
@@ -1,20 +1,6 @@
 ﻿using System;
-using System.Reflection;
 using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SharpSvn.PdbAnnotate")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SharpSvn Project")]
-[assembly: AssemblyProduct("SharpSvn.PdbAnnotate")]
-[assembly: AssemblyCopyright("Copyright (c) 2006-2021 SharpSvn Project")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -23,17 +9,3 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d59c4628-beba-4da8-ac54-cbd41b2a9759")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.8999.*")]
